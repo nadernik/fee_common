9a0b87b4d656b34b3f00cbdf92ef1d386650b58a
Michael Stetner <stetner@mit.edu>
2011-05-27 17:08:37 -0400

    x model caf working

--- xmodel/ratemodel_caf.m
+++ xmodel/ratemodel_caf.m
@@ -11,9 +11,9 @@ clear all
 
 % Time
 P.dt = 0.001; % seconds, time step
-P.thvc = 0.006; % seconds, length of hvc burst. sometimes I call this one time slice (slice is bigger than a step)
-P.tlman = 0.05; % seconds, timescale of fluctuations in lman
-P.motifs = 200; % number of motifs in simulation
+P.thvc = 0.005; % seconds, length of hvc burst. sometimes I call this one time slice (slice is bigger than a step)
+P.tlman = 0.08; % seconds, timescale of fluctuations in lman
+P.motifs = 1000; % number of motifs in simulation
 
 % Size of the network
 P.hvcunits = 50; % number of units in hvc
@@ -22,9 +22,9 @@ P.lmanunits = 1; % number of units in lman
 P.xunits = P.lmanunits * P.hvcunits; % number of units in x
 
 % Learning rates and other fudge factors
-P.xrate = .01; % learning rate in HVC->X synapse
+P.xrate = .004; % learning rate in HVC->X synapse
 P.rarate = 0; % learning rate in HVC->RA synapse
-P.rperate = 0.05; % learning rate of state value function V(s)
+P.rperate = .1; % learning rate of state value function V(s)
 P.discountrate = 0.98; % for reward prediction error. 0 means no history.
 
 P.noiseamp = 0.25;
@@ -69,6 +69,7 @@ W_ML = zeros(P.xunits, P.lmanunits);
 W_PM = zeros(P.lmanunits, P.xunits);
 W_DP = -eye(P.lmanunits); % inhibitory
 W_LD =  eye(P.lmanunits);
+
 W_RL = ones(P.raunits, P.lmanunits);
 W_RH = P.template;
 
@@ -99,15 +100,25 @@ for h = 1:P.hvcunits
     end
 end
 winit = W_MH;
+% W_PM(:,:) = 0; %%%DEBUG
 
 % Initialize empty matrices for activity in other neurons
 etrace = zeros(P.xunits, P.hvcunits);
-V = zeros(maxsteps + 1, P.hvcunits);
-temp = zeros(maxsteps, 1);
+V = zeros(1, P.hvcunits);
+Vall = zeros(P.motifs, P.hvcunits);
+dwall = zeros(maxsteps, P.xunits);
+wall = zeros(maxsteps, P.xunits);
 error = zeros(1,maxsteps +P.rewarddelay);
 rpe = zeros(maxsteps, 1);
+eall = zeros(maxsteps, P.xunits);
 %% Main loop
 for t = 1:maxsteps
+
+    % Reset elgigibility trace at the beginning of each motif
+%     if mod(t, motifsteps) == 0
+%         etrace = zeros(P.xunits, P.hvcunits);
+%     end
+
 
     % X activity is determined by input from HVC
     M(:, t) =  max(W_MH * H(:, t) - P.thresh, 0);
@@ -118,13 +129,17 @@ for t = 1:maxsteps
     D(:, t) = W_DP * PAL(:, t);
     
     % LMAN activity is the sum of intrinsic randomness and input from X.
-    L(:, t + 1) = randomness(:, t) + W_LD * D(:, t);
-    L(:, t + 1) = max(L(:, t + 1), 0);
+    L(:, t) = randomness(:, t) + W_LD * D(:, t);
+    L(:, t) = max(L(:, t), 0);
     % NOTE: that this LMAN input is used in NEXT time step
 
     % RA activity is the sum of inputs from HVC and LMAN
     R(:, t) = W_RH * H(:, t) + W_RL * L(:, t);
 
+%     if mod(t, hvcsteps) == 0
+%         continue
+%     end
+    
     % Calculate reward prediction error
     e  = (R(:, t) - P.template(:, s(t))).^2;
     if s(t) == P.caftime && R(:, t) < P.cafthresh
@@ -136,19 +151,27 @@ for t = 1:maxsteps
 
     % Update expected state value
     V(s(t)) = V(s(t)) + P.rperate * rpe(t);
+    Vall(ceil(t/motifsteps), s(t)) = V(s(t));
 
-    if t > 100*motifsteps % no learning for first 100 motifs
+    if t > 100*motifsteps% no learning for first 100 motifs
         % Update eligibility trace
         % Eligibility trace decays over time according to the discount rate and
         % receives an impulse when both the HVC and LMAN neuron . Eligibility
         % trace is specific to a single HVC->X synapse
         etrace = (((W_ML>0) * L(:, t)) * ones(1, P.hvcunits)) ...
-            .*    ((W_MH>0) .* (ones(P.xunits, 1) * H(:, t)')) ...
+            .*    ((W_MH~=0) .* (ones(P.xunits, 1) * H(:, t)')) ...
             + P.discountrate * etrace;
+        eall(t, :) = diag(etrace);
 
         % Update synaptic weights onto medium spiny neurons based on reward and
         % spiking history of LMAN and HVC neurons.
-        W_MH = W_MH + etrace .* rpe(t) .* P.xrate; % direct pathway
+        dw = etrace .* rpe(t) .* P.xrate;
+
+        %%%DEBUG
+%         dw(s(t), s(t)) = 0;
+        
+        W_MH = W_MH + dw; % direct pathway
+        dwall(t,:) = diag(dw);
 
         % Update HVC to RA synapses with a spike timing dependent plasticity
         % rule. If they both spike in this time step, the synapse is
@@ -160,10 +183,13 @@ for t = 1:maxsteps
         % Make sure none of the updated synaptic weights change sign.
         W_MH = max(W_MH, winit); % don't let weights on to MSNs go to zero
         %W_RH = max(W_RH, 0);
+        wall(t,:) = diag(W_MH);
+        
     end
 end
-save('charlesworthdemo.mat')
+%save('charlesworthdemo.mat')
 
+%% autocorrelation of LMAN fluctuations
 figure
 maxlag = 200;
 [c, lags] = xcorr(randomness, maxlag, 'coeff');
@@ -173,6 +199,7 @@ xlabel('Lag (ms)')
 ylabel('Autocorrelation, normalized')
 ylim([-0.2 1.2])
 
+%% learning (change in pitch) normalized
 figure
 t = 1:motifsteps;
 t = t - mean(t(s(t) == P.caftime));
@@ -186,7 +213,50 @@ ylabel('Learning, normalized')
 xlim([-160 160])
 ylim([-0.2 1.2])
 
+%% mean pitch traces beginning and ending (not normalized)
+figure
+n = 20; % number of trials to average
+Y = gettrials(R, -n:-1, motifsteps);
+final = mean(Y, 2); 
+Y = gettrials(R, 1:n, motifsteps);
+start = mean(Y, 2);
+plot(start, 'k')
+hold on
+plot(final, 'r')
 
+%% weights over time
+figure
+x = 1:50;
+y = linspace(0,P.motifs,size(wall,1));
+imagesc(x,y,wall)
+figure
+plot(wall(:, [11, 25, 34]))
+
+%% rpe-eligibility trace correlations at problem time
+figure
+Y = gettrials(rpe, 1:P.motifs, motifsteps);
+yrpe = Y(s(1:motifsteps) == 36, :);
+Y = gettrials(eall(:,35), 1:P.motifs, motifsteps);
+yetrace = Y(s(1:motifsteps) == 36, :);
+scatter(yrpe(:), yetrace(:))
+plot(yrpe(:))
+
+%%
+figure;
+Y = gettrials(rpe, 1:P.motifs, motifsteps);
+imagesc(Y')
+title('rpe')
+
+%%
+figure
+Y = gettrials(error, 1:P.motifs, motifsteps);
+imagesc(Y')
+title('error')
+
+%% average of rewarded lman fluctuations
+
+
+%%
 % Y = gettrials(rpe, 1:P.motifs, motifsteps);
 % t = 1:motifsteps;
 % target = mean(Y(s(t) == 34, :), 2);
