db9f6e592a7e81817a82ee500c49c053029b77d0
Michael Stetner <stetner@mit.edu>
2011-06-01 16:51:02 -0400

    changed variable names to appease michale
    --
    
    Expected reward no longer binned over whole HVC burst
    
    Expected reward (V) is now defined for every timestep of the motif (i.e.
    length(V) = motifsteps instead of length(V) = P.hvcunits).
    
    s(t) now just returns the current time mod the number of time steps in a motif.
    It used to return the HVC burst number we were in, but no longer.
    
    H, the matrix of HVC output, is now only defined for one motif. To get the
    HVC output at any time, look at H(:, s(t))
    
    Still operating open loop (DLM is disconnected from LMAN so LMAN never shows
    bias but maintains randomness).
    
    Discovered that CAF learning depends on length of punishment. For very short
    punishments the model does not learn because the signal-to-noise ratio

--- xmodel/ratemodel_caf.m
+++ xmodel/ratemodel_caf.m
@@ -10,183 +10,200 @@ clear all
 debugging = true;
 %% Parameters
 
-% Time
-P.dt = 0.001; % seconds, time step
-P.thvc = 0.005; % seconds, length of hvc burst. sometimes I call this one time slice (slice is bigger than a step)
-P.tlman = 0.2; % seconds, timescale of fluctuations in lman
-P.motifs = 500; % number of motifs in simulation
-P.baselinemotifs = 100; % number of motifs before learning starts
+total_motifs = 500; % number of motifs in simulation
+baseline_motifs = 100; % number of motifs before learning starts
 
 % Size of the network
-P.hvcunits = 50; % number of units in hvc
-P.raunits = 1;
-P.lmanunits = 1; % number of units in lman
-P.xunits = P.lmanunits * P.hvcunits; % number of units in x
+hvc_units = 50; % number of units in hvc
+ra_units = 1;
+lman_units = 1; % number of units in lman
+msn_units = lman_units * hvc_units; % number of units in x
+
+% Time
+dt = 0.001; % seconds, time step
+t_hvc = 0.005; % seconds, length of hvc burst. sometimes I call this one time slice (slice is bigger than a step)
+t_lman = 0.2; % seconds, timescale of fluctuations in lman
+
+hvc_steps = t_hvc / dt;
+motif_steps = hvc_steps * hvc_units;
+total_steps = motif_steps * total_motifs;
+
+
 
 % Learning rates and other fudge factors
-P.xrate = .008; % learning rate in HVC->X synapse
-P.rarate = 0; % learning rate in HVC->RA synapse
-P.rperate = .2; % learning rate of state value function V(s)
-P.discountrate = 0.98; % for reward prediction error. 0 means no history.
+msn_learning_rate = .01; % learning rate in HVC->X synapse
+ra_learning_rate = 0; % learning rate in HVC->RA synapse
+reward_learning_rate = .2; % learning rate of state value function V(s)
+eligibility_discount_rate = 0.98;
 
-P.noiseamp = 0.25;
-P.thresh = 0;
-P.radecay = 2e-3 * P.rarate;
-
-P.rewarddelay = 50; % 50 ms 
+randomness_amplitude = 0.25;
+msn_threshold = 0;
+ra_synapse_decay = 0;
+error_delay = 50; % time steps
 
 % conditional auditory feedback
-P.caftime = 25;
-P.cafthresh = 1;
-P.caferror = 3;
+caf_target_time = 125; % time steps
+caf_pitch_threshold = 1;
+caf_error_value = 3;
+caf_noise_duration = 5; % time steps
 
 % The template, aka the sequence we are trying to learn.
-% P.template = .5*ones(1, P.hvcunits);
-t = linspace(0, 1, P.hvcunits);
-P.template = -8e2 * t .* (t - 0.25).^2 .* (t - 0.7) .* (t - 0.8) .* (t - 1) + 0.1;
+t = linspace(0, 1, motif_steps);
+template = -8e2 * t .* (t - 0.25).^2 .* (t - 0.7) .* (t - 0.8) .* (t - 1) + 0.1;
 
-escapetrials = true(1, P.motifs);
 %% Initialize
 
-assert(size(P.template, 1) == P.raunits)
-assert(size(P.template, 2) == P.hvcunits)
-
-hvcsteps = P.thvc / P.dt; % number of time steps in each hvc burst
-motifsteps = hvcsteps * P.hvcunits; % number of time steps per motif
-maxsteps = motifsteps * P.motifs; % total number of time steps to simulate
-
 % "State" is the HVC unit that is currently firing.
-s = @(t) ceil(modnonzero(t, motifsteps)/hvcsteps);
+s = @(t) modnonzero(t, motif_steps);
 
 % Neural activity
-H = eye(P.hvcunits);
-L = zeros(P.lmanunits, maxsteps + 1);
-M = zeros(P.xunits,    maxsteps);
-PAL = zeros(P.lmanunits, maxsteps);
-D = zeros(P.lmanunits, maxsteps);
-R = zeros(P.raunits,   maxsteps);
+hvc_output = zeros(hvc_units, motif_steps);
+lman_output = zeros(lman_units, total_steps);
+msn_output = zeros(msn_units,    total_steps);
+pallidal_output = zeros(lman_units, total_steps);
+dlm_output = zeros(lman_units, total_steps);
+ra_output = zeros(ra_units,   total_steps);
 
 % Weights
-W_MH = zeros(P.xunits, P.hvcunits);
-W_ML = zeros(P.xunits, P.lmanunits);
-W_PM = zeros(P.lmanunits, P.xunits);
-W_DP = -eye(P.lmanunits); % inhibitory
-W_LD =  eye(P.lmanunits);
-W_RH = P.template;
+weights_on_msn_from_hvc = zeros(msn_units, hvc_units);
+weights_on_msn_from_lman = zeros(msn_units, lman_units);
+weights_on_pallidus_from_msn = zeros(lman_units, msn_units);
+weights_on_dlm_from_pallidus = -eye(lman_units); % inhibitory
+weights_on_lman_from_dlm =  eye(lman_units);
+hvc_centers = round((0.5:hvc_units-0.5)*hvc_steps); 
+weights_on_ra_from_hvc = template(hvc_centers);
+
+for t = 1:motif_steps
+    hvc_output(ceil(t/hvc_steps), t) = 1;
+end
 
 % Two LMAN units with opposite effects on the single RA neuron.
 % W_RL = [1, -1];
-W_RL = 1;
+weights_on_ra_from_lman = 1;
 
 % Make LMAN neurons fire randomly
-L = zeros(P.lmanunits, maxsteps + 1);
-randomness = zeros(P.lmanunits, maxsteps);
-for u = 1:P.lmanunits
-    randomness(u, :) = P.noiseamp * smoothnoise(maxsteps, P.tlman / P.dt);
+randomness = zeros(lman_units, total_steps);
+for u = 1:lman_units
+    randomness(u, :) = randomness_amplitude * smoothnoise(total_steps, t_lman / dt);
 end
 % randomness = max(randomness, 0);
-L(:, 1) = max(randomness(:, 1), 0);
+lman_output(:, 1) = max(randomness(:, 1), 0);
 
 % fill in weights for LMAN-X-DLM loop
 m = 0;
-for h = 1:P.hvcunits
-    for ell = 1:P.lmanunits
+for h = 1:hvc_units
+    for ell = 1:lman_units
         m = m + 1;
-        W_MH(m, h) = 1e-3; % start small. these weights are learned 
-        W_ML(m, ell) = 1;
-        W_PM(ell, m) = -1; % inhibitory
+        weights_on_msn_from_hvc(m, h) = 1e-3; % start small. these weights are learned 
+        weights_on_msn_from_lman(m, ell) = 1;
+        weights_on_pallidus_from_msn(ell, m) = -1; % inhibitory
     end
 end
-winit = W_MH;
+winit = weights_on_msn_from_hvc;
 
 % Initialize empty matrices for activity in other neurons
-etrace = zeros(P.xunits, P.hvcunits);
-V = zeros(1, P.hvcunits);
-error = zeros(1,maxsteps +P.rewarddelay);
-rpe = zeros(maxsteps, 1);
+eligibility_trace = zeros(msn_units, hvc_units);
+expected_reward = zeros(1, motif_steps);
+error = zeros(1,total_steps + error_delay);
 
 if debugging
-    Vall = zeros(P.motifs, P.hvcunits);
-    dw = zeros(size(W_MH));
-    dwall = zeros(maxsteps, P.xunits);
-    wall = zeros(maxsteps, P.xunits);
-    eall = zeros(maxsteps, P.xunits);
+    Vall = zeros(total_motifs, motif_steps);
+    dw = zeros(size(weights_on_msn_from_hvc));
+    dwall = zeros(total_steps, msn_units);
+    wall = zeros(total_steps, msn_units);
+    eall = zeros(total_steps, msn_units);
 end
-motiflabel = cell(1, P.motifs);
+is_escape = false(1, total_motifs);
+steps_to_noise = 0;
 %% Main loop
-for t = 1:maxsteps
-
+for t = 1:total_steps
+    current_motif = ceil(t / motif_steps);
+    
     % X activity is determined by input from HVC
-    M(:, t) =  max(W_MH * H(:, s(t)) - P.thresh, 0);
+    msn_input = weights_on_msn_from_hvc * hvc_output(:, s(t));
+    msn_output(:, t) = max(msn_input - msn_threshold, 0);
     % NOTE: LMAN has no immediate effect on HVC activity. Need to justify
     % this asymmmetry!
 
-    PAL(:, t) = W_PM * M(:, t); % Pallidal neurons
-    D(:, t) = W_DP * PAL(:, t); % DLM neurons
+    pallidal_input = weights_on_pallidus_from_msn * msn_output(:, t);
+    pallidal_output(:, t) = pallidal_input;
     
+    dlm_input = weights_on_dlm_from_pallidus * pallidal_output(:, t);
+    dlm_output = dlm_input;
+
     % LMAN activity is the sum of intrinsic randomness and input from DLM
-    L(:, t) = randomness(:, t);% + W_LD * D(:, t);
-    L(:, t) = max(L(:, t), 0); % Firing rates must be positive
+    lman_input = randomness(:, t);% + weights_on_lman_from_dlm * D(:, t);
+    lman_output(:, t) = max(lman_input, 0); % Firing rates must be positive
 
     % RA activity is the sum of inputs from HVC and LMAN
-    R(:, t) = W_RH * H(:, s(t)) + W_RL * L(:, t);
+    ra_input = weights_on_ra_from_hvc * hvc_output(:, s(t)) + weights_on_ra_from_lman * lman_output(:, t); 
+    ra_output(:, t) = ra_input;
     
-    % Calculate reward prediction error
-    e  = (R(:, t) - P.template(:, s(t))).^2; % error
-    if s(t) == P.caftime
-        tr = ceil(t / motifsteps);
-        if R(:, t) < P.cafthresh
-            e = P.caferror;
-            motiflabel{tr} = 'hit';
+    %% Auditory Feedback
+    
+    % Conditional auditory feedback
+    if s(t) == caf_target_time
+        if ra_output(1, t) < caf_pitch_threshold
+            % hit
+            steps_to_noise = caf_noise_duration;
+            is_escape(current_motif) = false;
         else
-            motiflabel{tr} = 'escape';
+            % escape
+            is_escape(current_motif) = true;
         end
-    else
-%         e = 0;
     end
     
-    error(t + P.rewarddelay) = e; % 
-    reward = -mean(error(t), 1); % averaged across all neurons
-    rpe(t) = reward - V(s(t));
+    % Instantaneous error
+    if steps_to_noise > 0
+        instantaneous_error = caf_error_value;
+        steps_to_noise = steps_to_noise - 1;
+    else
+        instantaneous_error = (ra_output(:, t) - template(:, s(t))).^2;
+    end
+    error(t + error_delay) = instantaneous_error;
 
-    % Update expected state value
-    V(s(t)) = V(s(t)) + P.rperate * rpe(t);
 
+    reward = -error(t);
+    rpe = reward - expected_reward(s(t));
 
-    if t > P.baselinemotifs*motifsteps% no learning for first 100 motifs
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    %%% Learning
+    
+    %
+    %% Update expected state value
+    expected_reward(s(t)) = expected_reward(s(t)) + reward_learning_rate * rpe;
+
+    %
+    %% Update synaptic weights only if we are past the baseline period
+    if current_motif > baseline_motifs
         % Update eligibility trace
         % Eligibility trace decays over time according to the discount rate and
         % receives an impulse when both the HVC and LMAN neuron . Eligibility
         % trace is specific to a single HVC->X synapse
-        etrace = (((W_ML>0) * L(:, t)) * ones(1, P.hvcunits)) ...
-            .*    ((W_MH~=0) .* (ones(P.xunits, 1) * H(:, s(t))')) ...
-            + P.discountrate * etrace;
+        eligibility_trace = (((weights_on_msn_from_lman>0) * lman_output(:,t)) * ones(1, hvc_units)) ...
+            .*    ((weights_on_msn_from_hvc~=0) .* (ones(msn_units, 1) * hvc_output(:, s(t))')) ...
+            + eligibility_discount_rate * eligibility_trace;
 
-        % Update synaptic weights onto medium spiny neurons based on reward and
-        % spiking history of LMAN and HVC neurons.
-        dw = etrace .* rpe(t) .* P.xrate;
-        W_MH = W_MH + dw; % direct pathway
 
-        % Update HVC to RA synapses with a spike timing dependent plasticity
-        % rule. If they both spike in this time step, the synapse is
-        % strengthened.
-        %W_RH  = W_RH + dw;
+        dw = eligibility_trace .* rpe .* msn_learning_rate;
+        weights_on_msn_from_hvc = weights_on_msn_from_hvc + dw;
+        % Make sure these synaptic weights are nonnegative.
+        weights_on_msn_from_hvc = max(0, weights_on_msn_from_hvc);
 
-        % Make sure none of the updated synaptic weights change sign.
-        W_MH = max(W_MH, winit); % don't let weights on to MSNs go to zero
-        %W_RH = max(W_RH, 0);
+%         dw = ra_output * hvc_output %%%FIXME
+%         weights_on_ra_from_hvc  = weights_on_ra_from_hvc + dw;
+
     end
     
     % Bookkeeping
     if debugging
-        Vall(ceil(t/motifsteps), s(t)) = V(s(t));
-        eall(t, :) = diag(etrace);
-        wall(t,:) = diag(W_MH);
+        Vall(ceil(t/motif_steps), s(t)) = expected_reward(s(t));
+        eall(t, :) = diag(eligibility_trace);
+        wall(t,:) = diag(weights_on_msn_from_hvc);
         dwall(t,:) = diag(dw);
     end
     
 end
-%save('charlesworthdemo.mat')
 
 %% autocorrelation of LMAN fluctuations
 figure
@@ -202,21 +219,21 @@ title('LMAN autocorrelation')
 %% learning (change in pitch) normalized
 
 % calculate actual learning
-pre  = gettrials(R,   1:20, motifsteps);
-post = gettrials(R, -20:-1, motifsteps);
+pre  = gettrials(ra_output,   1:20, motif_steps);
+post = gettrials(ra_output, -20:-1, motif_steps);
 learning = mean(post, 2) - mean(pre, 2);
 learning = learning ./ max(learning);
 
 % calculate predicted learning based on reinforced lman fluctuations
-mo = find(strcmpi('escape', motiflabel));
-mo = mo(mo < P.baselinemotifs);
-rewarded = gettrials(R, mo, motifsteps);
+mo = find(is_escape);
+mo = mo(mo < baseline_motifs);
+rewarded = gettrials(ra_output, mo, motif_steps);
 predicted = mean(rewarded, 2) - mean(pre, 2);
 predicted = predicted ./ max(predicted);
 
 figure
-t = 1:motifsteps;
-t = t - mean(t(s(t) == P.caftime));
+t = 1:motif_steps;
+t = t - mean(t(s(t) == caf_target_time));
 axes('FontSize', 16)
 plot(t, learning, 'k')
 hold on
@@ -229,8 +246,9 @@ title('Actual and predicted learning')
 
 % Overlayed pitch traces with target time highlighted
 figure
+axes('FontSize', 16)
 hold on
-pre = gettrials(R, 1:P.baselinemotifs, motifsteps);
+pre = gettrials(ra_output, 1:baseline_motifs, motif_steps);
 plot(pre, 'Color', [.8 .8 .8])
 plot(mean(pre, 2), 'k')
 ylo = min(min(pre));
@@ -238,64 +256,75 @@ yhi = max(max(pre));
 yrange = yhi - ylo;
 ylo = ylo - 0.15 * yrange;
 yhi = yhi + 0.15 * yrange;
-t = 1:motifsteps;
-xtarg = find(s(t) == P.caftime);
+t = 1:motif_steps;
+xtarg = find(s(t) == caf_target_time);
 X = [min(xtarg) max(xtarg) max(xtarg) min(xtarg)];
 Y = [ylo        ylo        yhi        yhi];
 fill(X, Y, [1 1 .7], 'FaceAlpha', 0.5)
-xlim([1 motifsteps])
+xlim([1 motif_steps])
 ylim([ylo yhi])
-xlabel('time')
+xlabel('Time (ms)')
 ylabel('"Pitch"')
 title('Songs, baseline')
 
 % histogram of pitches in target time
 figure
+axes('FontSize', 16)
 pitchtarg = pre(xtarg, :);
-hist(mean(pitchtarg), .2:0.05:1.6)
+hist(pitchtarg, .2:0.05:1.6)
 xlabel('"Pitch"')
 ylabel('N')
 title('Histogram of pitch at target time')
 %% mean pitch traces beginning and ending (not normalized)
-figure
-n = 20; % number of trials to average
-Y = gettrials(R, -n:-1, motifsteps);
-final = mean(Y, 2); 
-Y = gettrials(R, 1:n, motifsteps);
-start = mean(Y, 2);
-plot(start, 'k')
-hold on
-plot(final, 'r')
-title('Song, before and after')
+% figure
+% axes('FontSize', 16)
+% n = 20; % number of trials to average
+% Y = gettrials(R, -n:-1, motif_steps);
+% final = mean(Y, 2); 
+% Y = gettrials(R, 1:n, motif_steps);
+% start = mean(Y, 2);
+% plot(start, 'k')
+% hold on
+% plot(final, 'r')
+% xlabel('Time (ms)')
+% ylabel('"Pitch"')
+% title('Song, before and after')
 
 %% weights over time
 figure
+axes('FontSize', 16)
 x = 1:50;
-y = linspace(0,P.motifs,size(wall,1));
-imagesc(x,y,wall)
-figure
-plot(wall(:, [11, 25, 34]))
+y = linspace(0, total_motifs,size(wall,1));
+imagesc(x, y, wall)
+xlabel('HVC-X Synapse')
+ylabel('Motif')
+
+% figure
+% plot(wall(:, [11, 25, 34]))
 
 %%
 
 
 %% rpe-eligibility trace correlations at problem time
 % figure
-% Y = gettrials(rpe, 1:P.motifs, motifsteps);
-% yrpe = Y(s(1:motifsteps) == 36, :);
-% Y = gettrials(eall(:,35), 1:P.motifs, motifsteps);
-% yetrace = Y(s(1:motifsteps) == 36, :);
+% Y = gettrials(rpe, 1:P.motifs, motif_steps);
+% yrpe = Y(s(1:motif_steps) == 36, :);
+% Y = gettrials(eall(:,35), 1:P.motifs, motif_steps);
+% yetrace = Y(s(1:motif_steps) == 36, :);
 % scatter(yrpe(:), yetrace(:))
 % plot(yrpe(:))
 
 %% RPE over time
-figure;
-Y = gettrials(rpe, 1:P.motifs, motifsteps);
-imagesc(Y')
-title('rpe')
+% figure
+% axes('FontSize', 16)
+% Y = gettrials(rpe, 1:P.motifs, motif_steps);
+% imagesc(Y')
+% title('rpe')
+% xlabel('Time (ms)')
+% ylabel('Motif')
 
 %% Error over time
 % figure
-% Y = gettrials(error, 1:P.motifs, motifsteps);
+% Y = gettrials(error, 1:P.motifs, motif_steps);
 % imagesc(Y')
 % title('error')
