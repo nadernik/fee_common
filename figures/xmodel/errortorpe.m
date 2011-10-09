d7dc6ef5e78374880b1b40980dd218e6266c793a
Michael Stetner <stetner@mit.edu>
2011-10-09 18:25:01 -0400

    Reward prediction error figure -- start song as flat, bigger template

--- figures/xmodel/errortorpe.m
+++ figures/xmodel/errortorpe.m
@@ -16,8 +16,8 @@ colors.template = [1 1 0]; %yellow
 hist_ymax = 50;
 
 if exist('regen', 'var') && regen == true
-    template = sin(linspace(0, 4*pi, T)) + 4;
-    meansong = cos(linspace(0, 4*pi, T)) + 2;
+    template = 3*sin(linspace(0, 4*pi, T)) + 4;
+    meansong = zeros(size(template)) + 4;
 
     songs = meansong'*ones(1, N) + generate_lman_noise(T, N);
     errors = (songs - template'*ones(1, N)).^2;
@@ -36,13 +36,13 @@ bin_centers = -9.5:1:9.5;
 figure
 hold on
 plot(songs, 'Color', [.5 .5 1])
-plot(mean(songs, 2), 'Color', mean_color, 'LineWidth', 3)
+plot(mean(songs, 2), 'Color', colors.mean, 'LineWidth', 3)
 x = t + [-0.5, 0.5];
 y = interp1(1:T, songs(:, nred), x);
-plot(x, y, 'Color', bad_color, 'LineWidth', 3)
+plot(x, y, 'Color', colors.bad, 'LineWidth', 3)
 y = interp1(1:T, songs(:, ngreen), x);
-plot(x, y, 'g', 'Color', good_color, 'LineWidth', 3)
-plot(template, 'Color', template_color, 'LineWidth', 3)
+plot(x, y, 'g', 'Color', colors.good, 'LineWidth', 3)
+plot(template, 'Color', colors.template, 'LineWidth', 3)
 
 ylo = min(min(min(songs)), min(template));
 yhi = max(max(max(songs)), max(template));
@@ -55,12 +55,12 @@ axis off
 figure
 counts = hist(songs(t, :), bin_centers);
 h = histdot(counts, bin_centers);
-set(h, 'Marker', '.', 'MarkerEdgeColor', trace_color, 'MarkerFaceColor', trace_color)
+set(h, 'Marker', '.', 'MarkerEdgeColor', colors.trace, 'MarkerFaceColor', colors.trace)
 hold on
 [junk, redbin] = min(abs(bin_centers - songs(t, nred)));
-scatter(bin_centers(redbin), counts(redbin) + 1, 'MarkerEdgeColor', bad_color, 'MarkerFaceColor', bad_color)
+scatter(bin_centers(redbin), counts(redbin) + 1, 'MarkerEdgeColor', colors.bad, 'MarkerFaceColor', colors.bad)
 [junk, greenbin] = min(abs(bin_centers - songs(t, ngreen)));
-scatter(bin_centers(greenbin), counts(greenbin) + 1, 'MarkerEdgeColor', good_color, 'MarkerFaceColor', good_color)
+scatter(bin_centers(greenbin), counts(greenbin) + 1, 'MarkerEdgeColor', colors.good, 'MarkerFaceColor', colors.good)
 % scatter(nred, errors(t, nred), 300, '.r')
 % scatter(ngreen, errors(t, ngreen), 300, '.g')
 xlim([-10 10])
