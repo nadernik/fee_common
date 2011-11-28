0e1cf70ac68211b8075c12d63546d97c6d56b35d
Michael Stetner <stetner@mit.edu>
2011-11-28 14:27:27 -0500

    changed learning_from_successes.m to use simpler signals for hits/escapes

--- figures/xmodel/learning_from_successes.m
+++ figures/xmodel/learning_from_successes.m
@@ -6,22 +6,19 @@
 %%
 close all
 clear all
+
 xmodel_parameters_learning_from_successes;
 xmodel_initialize;
 
-%%
 is_escape = rand(1,total_motifs) < 0.5;
-t1 = 21;
-t2 = 81;
-mu = 0;
-t = -20:20;
-sigma = 5;
-p = normpdf(t,mu,sigma)*50;
+t1 = 80;
+t2 = 100;
+t = 0:20;
+p = sin(2*pi/40*t);
 esc = zeros(lman_units, motif_steps);
 hit = zeros(lman_units, motif_steps);
 esc(1,t2+t) = p; % on escape, pitch up LMAN neuron is active at t2
-hit(1,t1+t) = p; % on hit, pitch up neuron is active at t1 ...
-hit(2,t2+t) = p; % ... and pitch down neuron is active at t2
+hit(2,t1+t) = p; % on hit and pitch down neuron is active at t1
 for m = 1:total_motifs
     if is_escape(m)
         lman_noise(:,:,m) = esc;
@@ -30,15 +27,24 @@ for m = 1:total_motifs
     end
 end
 weights_on_lman_from_dlm = zeros(size(weights_on_lman_from_dlm));
-keyboard
 
-%%
 xmodel_run_learning_from_sucesses;
-save c:\stetner\data\figures\xmodel\learning_from_successes.mat
+save c:\stetner\data\figures\xmodel\learning_from_successes_1.mat
 
 %%
 clear all
-load c:\stetner\data\figures\xmodel\learning_from_successes.mat
+xmodel_parameters_learning_from_successes;
+xmodel_initialize;
+clear lman_noise is_escape
+load c:\stetner\data\figures\xmodel\learning_from_successes_1.mat lman_noise is_escape;
+is_escape = ~is_escape; % reverse which variants are successful
+weights_on_lman_from_dlm = zeros(size(weights_on_lman_from_dlm)); % turn off dlm to lman to prevent bias from showing
+xmodel_run_learning_from_sucesses;
+save c:\stetner\data\figures\xmodel\learning_from_successes_2.mat
+
+%%
+clear all
+load c:\stetner\data\figures\xmodel\learning_from_successes_2.mat
 
 %% Plots of escapes and hits
 % Overlay of all hits and escapes, with LMAN activity
