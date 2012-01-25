056a44d0987279c1fcf8754750659132420cf29a
Michael Stetner <stetner@mit.edu>
2012-01-25 13:05:53 -0500

    msn_output is now initialized to proper size in sparsemodel_initialize.m

--- xmodel/sparsemodel_initialize.m
+++ xmodel/sparsemodel_initialize.m
@@ -12,7 +12,7 @@ end
 % Initialize empty matrices for other units
 lman_input      = zeros(lman_units, motif_steps);
 lman_output     = zeros(lman_units, motif_steps, total_motifs);
-msn_output      = zeros(msn_units,  motif_steps);
+msn_output      = zeros(msn_units,  motif_steps, total_motifs);
 pallidal_output = zeros(lman_units, motif_steps, total_motifs);
 dlm_output      = zeros(lman_units, motif_steps, total_motifs);
 ra_output       = zeros(ra_units,   motif_steps, total_motifs);
