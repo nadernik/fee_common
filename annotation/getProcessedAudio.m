912df658dfc8cb38f55ebaf9b80c5b22e89011d6
Michael Stetner <stetner@mit.edu>
2012-01-25 15:16:40 -0500

    added getProcessedAudio.m, required for lman_fluctuations.m in earlier rev 7bbf27ecf60f3bf5cd34de243c3e7fea9a9f5131 (rev 127, "reinforcement learning paper figures_v1.pptx")

---
+++ annotation/getProcessedAudio.m
@@ -0,0 +1,84 @@
+function [audio, absTime, syllType, dura] = getProcessedAudio(birdName, varargin)
+
+	%Default Parameters
+	%To get files
+	P.experNames = []; %pass single or cell of experNames
+	P.prefix = 'all';
+	P.rootdir = 'c:\stetner\data\';
+	%To select syllables
+	P.targetSyll = [];
+	P.timeRanges = [];
+	P.selMode = {'none','randFrac'};
+	P.selParam = [];
+	%Other
+	P.targetRegion = [];
+	P.targetMethod = {'percent', 'time'};
+	P = parseargs(P, varargin{:});
+
+
+	[dMisc, folder] = getProcessedDataFiles(birdName, 'dataType', 'misc', 'experNames', P.experNames, 'prefix', P.prefix, 'rootdir', P.rootdir);
+	dPitch = getProcessedDataFiles(birdName, 'dataType', 'audio', 'experNames', P.experNames, 'prefix', P.prefix, 'rootdir', P.rootdir);
+	bSel = getProcessedSyllableSelection(dMisc, folder, 'targetSyll', P.targetSyll, 'timeRanges', P.timeRanges, 'selMode', P.selMode, 'selParam', P.selParam);
+	numSylls = sum(cellfun(@sum, bSel));
+
+	%load the data
+	audio = cell(numSylls,1);
+	absTime = nan(numSylls,1);
+	syllType = nan(numSylls,1);
+	dura = nan(numSylls,1);
+	currNdx = 0;
+	for nFile = 1:length(dMisc)
+		if(sum(bSel{nFile})>0)       
+			load([folder, dMisc(nFile).name]);
+			load([folder, dPitch(nFile).name]);
+			absTime(currNdx+1:currNdx+sum(bSel{nFile})) = [misc.segs(bSel{nFile}).absStart];
+			syllType(currNdx+1:currNdx+sum(bSel{nFile})) = [misc.segs(bSel{nFile}).segType];
+			dura(currNdx+1:currNdx+sum(bSel{nFile})) = [misc.segs(bSel{nFile}).duration];
+			audio(currNdx+1:currNdx+sum(bSel{nFile})) = {rawaudio.segs(bSel{nFile}).audio};
+			currNdx = currNdx + sum(bSel{nFile});
+		end
+	end
+
+	%sort the data
+	[absTime, sndx] = sort(absTime);
+	syllType = syllType(sndx);
+	dura = dura(sndx);
+	audio = audio(sndx);
+
+	%extract the targetRegion
+	if(~isempty(P.targetRegion))
+		if strcmp(P.targetMethod,'percent')
+			pitchTraj = cellfun(@extractFragment_percent, audio, repmat({P.targetRegion},length(audio),1), 'UniformOutput', false);
+		elseif strcmp(P.targetMethod,'time')
+            error('not implemented yet')
+ 			%pitchTraj = cellfun(@extractFragment_time, pitchTraj, repmat({fs},length(pitchTraj),1), repmat({P.},length(pitchTraj),1),repmat({P.targetRegion},length(pitchTraj),1)
+        end
+	end
+end
+
+function ts = extractFragment_percent(ts, syllFragment)
+	if(isempty(ts))
+		return;
+	end
+	ndxStart = ceil((length(ts)-1) * syllFragment(1)) + 1;
+	ndxEnd = floor((length(ts)-1) * syllFragment(2)) + 1;
+	ts = ts(ndxStart:ndxEnd);
+end
+
+function fragment = extractFragment_time(y, t_or_fs, t0, tlim)
+	% tlim in seconds
+	if isscalar(t_or_fs)
+		% if we are given a single number, it must be sampling rate
+		fs = t_or_fs;
+		t = 0:length(y)-1 * fs;
+	else
+		t = t_or_fs;
+	end
+
+	if t0 == 'end'
+		t0 = t(end);
+	end
+
+	tlim = tlim + t0;
+	fragment = y(t >= tlim(1) & t < tlim(end));
+end
