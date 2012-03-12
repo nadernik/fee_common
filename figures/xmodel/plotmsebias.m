a3f0261bcce5cd0d2363f863119422101c236c1c
Michael Stetner <stetner@mit.edu>
2012-03-12 01:59:21 -0400

    plotmsebias can return multiple arguments - plot handle, mean squared error

--- figures/xmodel/plotmsebias.m
+++ figures/xmodel/plotmsebias.m
@@ -1,6 +1,14 @@
-function plotmsebias(d)
+function varargout = plotmsebias(d)
 mse = xmodel_calculate_mse(permute(d.bias, [3 1 2]), d.template);
-plot(mse)
+h = plot(mse);
 xlabel('Trials')
 ylabel('Mean Squared Error')
+
+if nargout >= 1
+    varargout{1} = h;
+end
+if nargout >= 2
+    varargout{2} = mse;
+end
+
 end
