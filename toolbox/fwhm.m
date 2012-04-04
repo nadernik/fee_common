cdf97e168f64eb2e90477b6c8279879ef0a5fc66
Michael Stetner <stetner@mit.edu>
2012-04-04 14:22:06 -0400

    FWHM now returns NaN if signal never reaches half the maximum

--- toolbox/fwhm.m
+++ toolbox/fwhm.m
@@ -8,7 +8,12 @@ DEBUG_FLAG = 0;
 halfmax = xmax/2;
 i1 = find(x(imax-1:-1:1)<= halfmax, 1, 'first');
 i2 = find(x(imax+1:end) <= halfmax, 1, 'first');
-width = i1 + i2;
+
+if isempty(i1) || isempty(i2)
+    width = nan; % if x does not reach half max, fwhm is undefined
+else
+    width = i1 + i2;
+end
 
 if DEBUG_FLAG
     figure
