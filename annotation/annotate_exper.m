e65823b46c537f1bc45a98bbc9eb221f8b05a590
Michael Stetner <stetner@mit.edu>
2012-12-10 18:44:20 -0500

    added documentation for annotate_exper

--- annotation/annotate_exper.m
+++ annotation/annotate_exper.m
@@ -1,14 +1,47 @@
 function annotate_exper(birdname, expername, varargin)
+%ANNOTATE_EXPER Segment audio into syllables and calculate features
+%Audio files must be part of an acquisitionGui experiment. This will
+%segment the audio files into syllables based on a fixed threshold that you
+%specify and 
+%
+%Syntax:
+% annotate_exper(birdname, expername)
+% annotate_exper(birdname, expername, 'ParameterName', value, ...)
+%
+%List of parameters:
+%   rootdir = Root directory for acquisitionGui. Your exper.mat should be
+%             located in rootdir/birdname/expername.
+%   filenum = File numbers to annotate. Leave blank for all files (default)
+%   bDebug = If set to true, shows spectrograms of files to help you set
+%            segmentation parameters.
+%   maxFilesPerAnnotation = number of acquisitionGui files to store in each
+%                           annotation file. Decrease this number if you
+%                           are running out of memory in vectorClust.
+%   triggerSyllThreshold = Every syllable must cross this threshold at
+%                          least once.
+%   edgeSyllThreshold = Threshold to detect syllable onsets and offsets.
+%   fMinSyllDuration = Minimum syllable duration in seconds
+%   fMinIntervalDuration = Minimum gap duration in seconds
+%   fMaxSyllDuration = Maximum syllable duration in seconds
+
+
+%% Default parameter values. 
+% All of these values can be set by supplying the parameter name (as a string) 
+% and a parameter value as arguments to annotate_exper(). For example: 
+% annotate_exper(birdname, expername, 'rootdir', 'c:\path\to\data')
 
 P.rootdir = 'c:\stetner\data\';
-P.triggerSyllThreshold = -11; % threshold to detect middle of syllable
-P.edgeSyllThreshold = -14; % threhold to detect beginning and end of syllables
-P.filenum = []; % files to annotate. leave blank to annotate all files.
-P.bDebug = false; % show spectrogram of segmented files?
-P.maxFilesPerAnnotation = 300;
+P.filenum = []; % blank to annotate all files
+P.bDebug = false; 
+P.maxFilesPerAnnotation = 300; 
+
+% Segmentation parameters
+P.triggerSyllThreshold = -11; 
+P.edgeSyllThreshold = -14; 
 P.fMinSyllDuration = .016; %secs
 P.fMinIntervalDuration = .007; %secs
 P.fMaxSyllDuration = 1; %sec
+
 P = parseargs(P,varargin{:});
 
 %%
