5b38ef3a340a14332b2f4afa82a4c6b1c5dcd79b
Michael Stetner <stetner@mit.edu>
2012-12-19 14:35:07 -0500

    Removed recurrence - MSN output is now independent of LMAN

--- xmodel/SparseNet.m
+++ xmodel/SparseNet.m
@@ -36,7 +36,7 @@ classdef SparseNet < handle
             obj.msnout = zeros(obj.nmsn, obj.nhvc, obj.niter);
             obj.lmanout = zeros(obj.nhvc, obj.niter);
             obj.wH = obj.winit * rand(obj.nmsn,obj.nhvc);
-            obj.template = 10*sin(linspace(0,2*pi,obj.nhvc)) + 20;
+            obj.template = sin(linspace(0,2*pi,obj.nhvc)) + 1;
             obj.rexp = zeros(obj.nhvc, obj.niter);
             %obj.noise = rand(obj.nhvc, obj.niter);
             %obj.noise = max(0,randn(obj.nhvc, obj.niter)/8+.5);
@@ -54,13 +54,11 @@ classdef SparseNet < handle
         end
         
         function ffstep(obj, iter)
-            iter
             % MSN activity depends on HVC input and noise (from lman)
-            msnin = obj.wH * obj.hvcout + ...
-                ones(obj.nmsn,1) * obj.noise(:,iter)' - obj.tinhib;
+            msnin = obj.wH * obj.hvcout - obj.tinhib;
             % MSN output is threshold linear
             obj.msnout(:,:,iter) = max(0, msnin);
-            obj.lmanout(:,iter) = mean(obj.msnout(:,:,iter), 1)' + ...
+            obj.lmanout(:,iter) = sum(obj.msnout(:,:,iter), 1)' + ...
                 obj.noise(:,iter);
         end
         
@@ -97,7 +95,7 @@ classdef SparseNet < handle
         end
         
         function inhib = allinhib(obj, iter)
-            inhib = obj.tinhib + obj.istr * mean(obj.msnout(:,:,iter), 1);
+            inhib = obj.tinhib + obj.istr * sum(obj.msnout(:,:,iter), 1);
             %inhib = zeros(size(inhib)); %FIXME
         end
         
--- xmodel/sparsenet_test.m
+++ xmodel/sparsenet_test.m
@@ -1,18 +1,21 @@
+%%
 close all
 clear all
+%%
 sn = SparseNet();
-sn.niter = 5e3;
-sn.tinhib = 1;
-sn.niter = 1000;
-sn.LTPrate = 3.5e-4;
+%%
 sn.nhvc = 100;
 sn.nmsn = 500;
-sn.LTDrate = 1.0e-3;
+sn.niter = 2000;
+%%
+sn.LTPrate = 1e-1;
+sn.LTDrate = 0;%1e-2;
 sn.tinhib = 1;
+%%
 sn.init()
 sn.simulate()
-
-clf
+%%
+figure
 clf
 subplot(1,3,1)
 sn.wimage
@@ -22,7 +25,7 @@ learning = mean(lastsongs, 2);
 plot(learning)
 hold all
 plot(sn.template)
-ylim([0, 40])
+ylim([0, 5])
 xlabel('Time (ms)')
 ylabel('Pitch')
 legend('Learned Song', 'Template')
