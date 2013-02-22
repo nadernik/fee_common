4c37f42a762d5465ddb9635e9c8f6dc66b999bad
Michael Stetner <stetner@mit.edu>
2013-02-22 14:21:57 -0500

    Area X Modeling -- Added figure showing that learning is still the average of the baseline escapes, even with RA->DLM connection
    One of the key findings of my modeling work is that LMAN gating plasticity in
    MSNs explains how the temporal resolution of learning looks like the average of
    the escapes. This result still holds in a modified model where there is a
    connection from RA to DLM.

---
+++ xmodel/figures/figure_learning_is_average_of_baseline_escapes_radlm.m
@@ -0,0 +1,145 @@
+function figure_learning_is_average_of_baseline_escapes_radlm(do_simulations)
+
+datafile = 'c:\stetner\data\figures\xmodel\caf_radlm3.mat';
+colorscheme = 'C:\stetner\code\figures\xmodel\xmodel_color_scheme.mat';
+num_traces = 10;
+
+%% Simulate data if requested
+% If function is called without an argument or with an argument that is not
+% 1, this step is skipped!
+if exist('do_simulations', 'var') && (do_simulations == 1)
+    xmodel_parameters_caf
+    baseline_motifs = 1000;
+    learning_motifs = 600;
+    total_motifs = baseline_motifs + learning_motifs + ending_motifs;
+    ra_dlm_strength = 0.2;
+    ra_noise_amplitude = 0.6;
+    xmodel_initialize_ra_to_dlm
+    xmodel_run_ra_to_dlm_slow
+    save(datafile)
+end
+
+%% Load stuff
+
+d = load(datafile); % Load data
+d.bias = squeeze(d.bias);
+c = load(colorscheme);
+
+
+%% Average of baseline escapes
+
+figure
+a1 = subplot(2,1,1); 
+is_baseline = (1:d.total_motifs) <= d.baseline_motifs;
+baseline_escapes = squeeze(d.pitch(:,is_baseline &  d.is_escape));
+baseline_hits    = squeeze(d.pitch(:,is_baseline & ~d.is_escape));
+
+% example hits
+hold on
+hitmotif = size(baseline_hits,2) - (0:num_traces-1);%randsample(size(baseline_hits,2), num_traces);
+plot(baseline_hits(:,hitmotif), 'Color', c.hit)
+
+% example escapes
+escmotif = size(baseline_escapes,2) - (0:num_traces-1);%randsample(size(baseline_escapes,2), num_traces);
+plot(baseline_escapes(:,escmotif), 'Color', c.escape)
+
+% average of baseline escapes
+avg_of_baseline_escapes = mean(baseline_escapes, 2);
+fprintf('There are a total of %g baseline escapes and %g baseline hits.\n', size(baseline_escapes,2), size(baseline_hits, 2))
+plot(avg_of_baseline_escapes, 'Color', c.escape, 'LineWidth', 3)
+xlim([1 (d.motif_steps + d.extra_steps)])
+set(gca, 'XTick', [])
+set(gca, 'FontSize', 16)
+ylabel('Song (% \Delta pitch)')
+
+
+%% Reward prediction error examples
+rpe = d.reward - d.expected_reward(:,1:d.total_motifs);
+rpe_hit = rpe(:,is_baseline & ~d.is_escape);
+rpe_esc = rpe(:,is_baseline &  d.is_escape);
+
+subplot(2,1,2)
+plot(rpe_hit(:,hitmotif), 'Color', c.hit)
+hold on
+plot(rpe_esc(:,escmotif), 'Color', c.escape)
+xlim([1 (d.motif_steps + d.extra_steps)])
+set(gca, 'FontSize', 16)
+xlabel('Time (ms)')
+ylabel('RPE')
+
+%% Bias before and after learning
+figure
+
+% Calculate the bias at the end of learning by running the simulation
+% without noise. We cannot just calculate the 
+for t = 1:d.motif_steps
+    msn_input = d.weights_on_msn_from_hvc * d.hvc_output(:,t);
+    msn_output(:,t) = max(msn_input - d.msn_threshold, 0);
+    
+    pallidal_output(:,t) = d.weights_on_pallidus_from_msn * msn_output(:,t);
+    
+    if t == 1
+        dlm_output(:,t) = d.weights_on_dlm_from_pallidus * pallidal_output(:,t);
+    else
+        dlm_output(:,t) = d.weights_on_dlm_from_pallidus * pallidal_output(:,t) + ...
+            d.weights_on_dlm_from_ra * ra_output(:,t-1);
+    end
+    
+    lman_input = d.lman_offset + d.weights_on_lman_from_dlm * dlm_output(:,t);
+    lman_output(:,t) = lman_input; %max(0, lman_input); %DEBUG
+    ra_input = d.weights_on_ra_from_lman * lman_output(:,t) + ...
+        d.weights_on_ra_from_hvc * d.hvc_output(:,t) + d.lman_offset;
+    ra_output(:,t) = ra_input; %max(0, ra_input); %DEBUG
+    bias_end(t) = d.weights_on_song_from_ra * ra_output(:,t);
+end
+
+% normalize so that the max of the bias after learning is 1.
+Z = max(bias_end); % normalization constant
+% plot(fixme/Z, ':', 'Color', c.bias, 'LineWidth', 3)
+hold on
+plot(bias_end/Z, 'Color', c.bias, 'LineWidth', 3)
+set(gca, 'FontSize', 16)
+xlabel('Time (ms)')
+
+% Full width at half maximum of bias after learning
+[width, xw] = fwhm(bias_end);
+y = [0.5, 0.5];
+[fx, fy] = dsxy2figxy(xw, y);
+annotation('doublearrow',fx,fy)
+str = sprintf('%.1f ms', width);
+text(xw(2)+width, 0.5, str);
+
+set(gca, 'YTick', [0 1])
+%% Plot bias after learning on top of HVC burst, average of basline
+%% escapes, and reward kernel
+
+% bias after learning
+figure
+t = (1:d.motif_steps) - d.caf_target_time2;
+plot(t, bias_end./max(bias_end), 'Color', c.bias, 'LineWidth', 3)
+hold on
+fprintf('Full width at half maximum of bias after learning is %g ms.\n', fwhm(bias_end))
+
+% hvc burst
+hvc_burst = d.hvc_output(1, 1:9);
+t = 1:length(hvc_burst);
+t = t-mean(t);
+plot(t, hvc_burst./max(hvc_burst), 'Color', c.hvc, 'LineWidth', 3)
+fprintf('Full width at half maximum of HVC burst is %g ms.\n', fwhm(hvc_burst))
+
+% average of baseline escapes
+t = (1:d.motif_steps) - d.caf_target_time2;
+plot(t, avg_of_baseline_escapes./max(avg_of_baseline_escapes), 'Color', c.escape, 'LineWidth', 3)
+fprintf('Full width at half maximum of average of baseline escapes is %g ms.\n', fwhm(avg_of_baseline_escapes))
+
+% reward kernel
+t = 1:length(d.rkernel);
+t = t-mean(t);
+plot(t, d.rkernel./max(d.rkernel), 'Color', c.vta, 'LineWidth', 3)
+fprintf('Full width at half maximum of reward kernel is %g ms.\n', fwhm(d.rkernel))
+
+set(gca, 'FontSize', 16, 'YTick', [0 1])
+xlabel('Time from CAF target (ms)')
+legend({'Learning', 'HVC burst', 'Baseline escapes', 'Reward kernel'})
+
+keyboard
