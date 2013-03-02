01a442ceac541e16bfddfde16b62b5a7042a99a1
Emily Mackevicius <elm@mit.edu>
2013-03-02 16:11:37 -0500

    "planning out string pull code"

--- play_songs_elm/StringPull.m
+++ play_songs_elm/StringPull.m
@@ -1,3 +1,12 @@
+
+% load songs to play
+[simple,fs] = wavread('CallIntroFast.wav');
+
+% check they all have the same sampling rates
+
+% make a cell array of what songs to to each box
+WhichBox = {[simple zeros(numel(simple),1)] [zeros(numel(simple),1) simple] [simple simple]};
+
 % initialize dio and ao
 dio = digitalio('nidaq','Dev1');
 addline(dio,0,'in');
@@ -11,12 +20,19 @@ addchannel(ao, 1);
 set(ao, 'SampleRate', fs)
 set(ao, 'TriggerType', 'Manual');
 
+% while it's today
+
 % get value of the dio
 pushed = getvalue(dio);
 
+% check if each is pressed, and
+
+% play data based on this, if it hasn't reached the maximum
 putdata(ao, WhichBox{1})
 start(ao)
 trigger(ao)
 wait(ao,5)
 stop(ao)
+
+% keep track of when the button was pressed for each cooler
                 
