c7684be38711b345f6e1bc9410c1c0109e20b0d2
Emily Mackevicius <elm@mit.edu>
2013-03-08 11:28:16 -0500

    StringPull now emails daily results

--- play_songs_elm/StringPull.m
+++ play_songs_elm/StringPull.m
@@ -1,9 +1,11 @@
+%%
 close all; clear all; clc
 
 try
+for i = 1:20
     % load songs to play
     [slowed,fs] = wavread('Z:\VocodedSongs\slowed.wav');
-    [sped,fs1] = wavread('Z:\VocodedSongs\slowed.wav');
+    [sped,fs1] = wavread('Z:\VocodedSongs\sped.wav');
 
     % check they all have the same sampling rates
     fs 
@@ -16,14 +18,14 @@ try
     % length
     song{1,2} = slowed; % cooler 1 song
     song{1,1} = zeros(numel(slowed),1); % cooler 1 silence
-    song{2,2} = [sped zeros(numel(slowed)-numel(sped),1)]; % cooler 2 song
+    song{2,2} = [sped; zeros(numel(slowed)-numel(sped),1)]; % cooler 2 song
     song{2,1} = zeros(numel(slowed),1); % cooler 2 silence
 
     % initialize dio and ao
     dio = digitalio('nidaq','Dev1');
     addline(dio,0,'in');
     addline(dio,1,'in');
-    pulled = getvalue(dio); pulled(1) = ~pulled(1); % switch on channel 0 is 1 when open
+    pulled = getvalue(dio); 
     ao = analogoutput('nidaq', 'Dev1');
     addchannel(ao, 0);
     set(ao, 'SampleRate', fs)
@@ -42,8 +44,9 @@ try
     today = date;
     % is there a way of doing this with timers or interrupts??
     while issame(today(1:2),date(1:2))
+        date = datestr(now);
         % get value of the dio
-        pulled = getvalue(dio); pulled(1) = ~pulled(1) % switch on channel 0 is 1 when open
+        pulled = getvalue(dio);
         % check if each is pressed, and
         if sum(pulled) ~= 0
             % keep track of pull times
@@ -57,12 +60,12 @@ try
             if Nplays(1) == 0
                 pulled(1) = 0;
             elseif pulled(1)
-                Nplays(1) = Nplays(1) - 1;
+                Nplays(1) = Nplays(1) - 1
             end
             if Nplays(2) == 0
                 pulled(2) = 0; 
             elseif pulled(2)
-                Nplays(2) = Nplays(2) - 1;
+                Nplays(2) = Nplays(2) - 1
             end
             play = [song{1,pulled(1)+1}, song{2,pulled(2)+1}];
             % play data based on this, if it hasn't reached the maximum
@@ -73,7 +76,9 @@ try
             stop(ao)
         end
         save(['Z:\PullTimes\PullTimes', num2str(round(now*1000)), '.mat'])
+        emailme(['today strings were pulled ', [num2str(40-Nplays(1))], ' and ', [num2str(40-Nplays(2))], ' times'])
     end
+end
 catch 
     A = lasterror;
     save(['Z:\PullTimes\PullTimes', num2str(round(now*1000)), '.mat'])
