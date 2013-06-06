cef76f6b83413eaf7de91479ea7b3f31287ea633
Emily Mackevicius <elm@mit.edu>
2013-06-06 17:45:42 -0400

    adding .m file to load acqGUI sounds, and correct dir

---
+++ misc_elm/load_acqGUI_sound.m
@@ -0,0 +1,22 @@
+dir = 'C:/users/emackev/Documents/MATLAB/AcqGUI/';
+bird = '3765';
+exper = '2013-06-06';
+num = 1; 
+
+exp = loadExper(bird, exper, dir); 
+exp.dir = [fullfile(dir, bird, exper) '\'];
+
+audio = loadAudio(exp, num); 
+fs = exp.desiredInSampRate;
+%% display sound
+figure; 
+displaySpecgramQuick(audio, fs)
+sound(audio, fs)
+%% vocode sound
+orig_dur = length(orig_sound)/fs; 
+desired_dur = 1/7;
+
+desired_sound = pvoc(orig_sound, orig_dur/desired_dur, 200); 
+
+displaySpecgramQuick(desired_sound, fs)
+sound(desired_sound,fs)
