3e2bab7f8c96355eb02e5f7cb9de6068fe43a111
Emily Mackevicius <elm@mit.edu>
2013-06-25 09:50:13 -0400

    .mfile that removes the gaps from a song, and flattens it

---
+++ misc_elm/RemoveGapsFlatten.m
@@ -0,0 +1,131 @@
+
+%% making new songs
+clear all; close all
+%syllable library
+path = 'C:\Users\emackev\Documents\MATLAB\OferSongs';
+[bells,fs] = wavread(fullfile(path, 'bells')); 
+[simple,fs] = wavread(fullfile(path, 'simple')); 
+[song,fs] = wavread(fullfile(path, 'bells')); 
+A = bells(.675*fs:.747*fs);
+C = bells(.242*fs:.301*fs);
+D = bells(.34*fs:.384*fs);
+B = simple(1.076*fs:1.195*fs);
+
+%set parameters
+SongLength = round(.5*fs); 
+cosramp = round(.005*fs); 
+Gap = round(.03*fs);
+NoGaps = 1;
+Flattened = 1; 
+IMotifI = .1*fs;
+scalefactor = 20;
+normalizeSyls = 1;
+
+%decide order
+Syl{1} = A;
+Syl{2} = B;
+Syl{3} = C;
+Syl{4} = D; 
+
+PrevOnset = 0; 
+PrevDur = 0; 
+Seed = zeros(numel(Syl), SongLength);
+for syli = 1:numel(Syl)
+    S = Syl{syli};
+    if ~NoGaps
+        OnsetS = PrevOnset+PrevDur+Gap;
+    elseif syli ~= 1;
+        OnsetS = PrevOnset+PrevDur-2*(cosramp);
+    else
+        OnsetS = 0;
+    end
+    PrevOnset = OnsetS; 
+    PrevDur = numel(S);
+    if normalizeSyls
+        S2 = pvocnormalized(S,1,200);
+        S = S*max(S2)/max(S);
+    end
+    window = [.5 - (cos(0:(pi/(cosramp)):pi)/2) ones(1,numel(S)-2*cosramp-2) ...
+        .5+(cos(0:(pi/(cosramp)):pi)/2)]';
+    Seed(syli,:) = [zeros(1, OnsetS) (S.*window)' zeros(1, (SongLength-OnsetS)-numel(S))];
+end
+
+All = sum(Seed);
+All = All(1:PrevOnset+PrevDur);
+if Flattened
+    All = pvocnormalized(All, 1,200);
+    All = All';
+end
+All = [zeros(1,IMotifI ) All zeros(1,IMotifI) All zeros(1,IMotifI)];
+All = All*scalefactor;
+subplot(211)
+plot(1/fs:1/fs:numel(All)/fs, All)
+subplot(212)
+displaySpecgramQuick(All,fs)
+sound(All,fs)
+shg
+
+
+%% gapfreeing and flattening another song
+close all; clear all
+path = 'C:\Users\emackev\Documents\MATLAB\Tutors'
+[song,fs] = wavread(fullfile(path, 'Purple40_3803_cage42_tutor.wav')); 
+
+%set syllables
+%segment in electrogui, set high threshold so that you don't include any
+%preonset gap (don't want to flatten up the noise)
+load(fullfile(path, 'analysis_purple40'));%load(fullfile(path, 'analysis_simple_bp_860_8600'));
+segs = dbase.SegmentTimes{1}; 
+
+for i = 1:size(segs,1)
+    Syl{i} = song(segs(i,1):segs(i,2));
+end
+
+%set parameters
+SongLength = round(2*fs); 
+cosramp = round(.005*fs); 
+Gap = round(.03*fs);
+NoGaps = 1;
+Flattened = 1; 
+IMotifI = .1*fs;
+scalefactor = 15;
+normalizeSyls = 0; 
+
+
+PrevOnset = 0; 
+PrevDur = 0; 
+Seed = zeros(numel(Syl), SongLength);
+for syli = 1:numel(Syl)
+    S = Syl{syli};
+    if ~NoGaps
+        OnsetS = PrevOnset+PrevDur+Gap;
+    elseif syli ~= 1;
+        OnsetS = PrevOnset+PrevDur-2*(cosramp);
+    else
+        OnsetS = 0;
+    end
+    PrevOnset = OnsetS; 
+    PrevDur = numel(S);
+    if normalizeSyls
+        S2 = pvocnormalized(S,1,200);
+        S = S*max(S2)/max(S);
+    end
+    window = [.5 - (cos(0:(pi/(cosramp)):pi)/2) ones(1,numel(S)-2*cosramp-2) ...
+        .5+(cos(0:(pi/(cosramp)):pi)/2)]';
+    Seed(syli,:) = [zeros(1, OnsetS) (S.*window)' zeros(1, (SongLength-OnsetS)-numel(S))];
+end
+
+All = sum(Seed);
+All = All(1:PrevOnset+PrevDur);
+if Flattened
+    All = pvocnormalized(All, 1,200);
+    All = All';
+end
+All = [zeros(1,IMotifI ) All zeros(1,IMotifI) All zeros(1,IMotifI)];
+All = All*scalefactor;
+subplot(211)
+plot(1/fs:1/fs:numel(All)/fs, All)
+subplot(212)
+displaySpecgramQuick(All,fs)
+sound(All,fs)
+shg
