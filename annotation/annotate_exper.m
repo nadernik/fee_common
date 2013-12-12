963663c3df98437c17f8bc110a8afe20a9a37fa2
Michael Stetner <stetner@mit.edu>
2013-12-12 11:58:48 -0500

    Fixed the way filenames are handled in annotate_exper (using fullfile function)

--- annotation/annotate_exper.m
+++ annotation/annotate_exper.m
@@ -11,12 +11,16 @@ function annotate_exper(birdname, expername, varargin)
 %List of parameters:
 %   rootdir = Root directory for acquisitionGui. Your exper.mat should be
 %             located in rootdir/birdname/expername.
-%   filenum = File numbers to annotate. Leave blank for all files (default)
+%             (default = 'c:\stetner\data\')
+%   filenum = File numbers to annotate. Empty matrix for all files
+%             (default = [])
 %   bDebug = If set to true, shows spectrograms of files to help you set
 %            segmentation parameters.
+%            (default = false)
 %   maxFilesPerAnnotation = number of acquisitionGui files to store in each
 %                           annotation file. Decrease this number if you
 %                           are running out of memory in vectorClust.
+%
 %   triggerSyllThreshold = Every syllable must cross this threshold at
 %                          least once.
 %   edgeSyllThreshold = Threshold to detect syllable onsets and offsets.
@@ -29,9 +33,10 @@ function annotate_exper(birdname, expername, varargin)
 % All of these values can be set by supplying the parameter name (as a string) 
 % and a parameter value as arguments to annotate_exper(). For example: 
 % annotate_exper(birdname, expername, 'rootdir', 'c:\path\to\data')
+% IF YOU CHANGE THE DEFAULT VALUES, PLEASE UPDATE THE HOWTO DOCUMENT!
 
 P.rootdir = 'c:\stetner\data\';
-P.filenum = []; % blank to annotate all files
+P.filenum = []; % empty matrix to annotate all files
 P.bDebug = false; 
 P.maxFilesPerAnnotation = 300; 
 
@@ -45,11 +50,17 @@ P.fMaxSyllDuration = 1; %sec
 P = parseargs(P,varargin{:});
 
 %%
+
+if P.bDebug == true && ~isscalar(P.filenum)
+    error('When bDebug is true, you must use the filenum parameter to select only one file.')
+end
+
 P.filenum = {P.filenum};
 
 expers{1} = loadExper(birdname, expername, P.rootdir);
-expers{1}.dir = [P.rootdir birdname filesep expername filesep];
-annotationName = [P.rootdir filesep birdname filesep birdname '_annotation_' expername '.mat'];
+expers{1}.dir = fullfile(P.rootdir, birdname, expername);
+filename = [birdname '_annotation_' expername '.mat'];
+annotationName = fullfile(P.rootdir, birdname, filename);
 
 %segment
 annotNames = caf_ProcessExperAudio(annotationName, expers, P.filenum, ...
