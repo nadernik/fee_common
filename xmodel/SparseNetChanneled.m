a5dc0df0f98214b810adcef9b22df77c99bfeca8
Michael Stetner <stetner@mit.edu>
2013-12-12 11:59:40 -0500

    Hard-wired model now only does weight update calculation for weights that are allowed to change.

--- xmodel/SparseNetChanneled.m
+++ xmodel/SparseNetChanneled.m
@@ -8,24 +8,23 @@ classdef SparseNetChanneled < SparseNetHardwired
         nlman = 2; % number of lman channels
         vocalout % vocal output
         nvocal = 1; % number of dimensions in vocal output
+        wtoupdate % list of weights to update
     end
     
     methods
         function init(obj)
-            % Make sure we have the right numbers of LMAN and MSN units.
+            % Set the right numbers of LMAN and MSN units.
             % Each dimension of vocal output must have 2 LMAN units (one to
             % push and one to pull). Each LMAN unit must in turn have one
             % MSN unit for each HVC unit because HVC->MSN will be wired
             % one-to-one in each channel.
-            if obj.nlman ~= (2 * obj.nvocal)
-                error('SparseNet:badParameter', 'Must have 2 LMAN units for each dimension of vocal output.')
-            end
-            if obj.nmsn ~= (obj.nlman * obj.nhvc)
-                error('SparseNet:badParameter', 'Must have exactly lman*hvc MSN units.')
-            end
+            obj.nlman = 2 * obj.nvocal;
+            obj.nmsn = obj.nlman * obj.nhvc;
+
             
             % HVC burst is one period of sine squared, scaled so that its
             % area is unity.
+            obj.hvcout = zeros(obj.nhvc);
             BL = obj.hvcburstlen + 2; % add 2 to burst length because the first and last points of the burst will be zero
             t = linspace(0, pi, BL);
             hvcburst = sin(t).^2; 
@@ -53,6 +52,7 @@ classdef SparseNetChanneled < SparseNetHardwired
             % distinct subset of MSN units and receives input from these
             % same MSN units. 
             chanmsn = floor(obj.nmsn/obj.nlman); % number of msn units in each channel
+            obj.wL = zeros(obj.nmsn, obj.nlman);
             for ch = 1:obj.nlman
                 imsn = (1:chanmsn) + chanmsn*(ch-1);
                 obj.wL(imsn,ch) = 1;
@@ -72,6 +72,22 @@ classdef SparseNetChanneled < SparseNetHardwired
             obj.wH = zeros(obj.nmsn, obj.nhvc, obj.niter);
             obj.wH(:,:,1) = repmat(eye(obj.nhvc), obj.nlman, 1);
             
+            % List of weights to update. The matrix wH contains all
+            % possible connections between HVC and MSNs, but in this model
+            % HVC is wired one-to-one to MSNs. We can save time by updating
+            % only these weights that exist. This is a list of those
+            % weights - the ones that are nonzero initially.
+            wlist = find(obj.wH(:,:,1) > 0); % list of weights to update
+            siz = size(obj.wH(:,:,1));
+            for iw = 1:length(wlist)
+                [m h] = ind2sub(siz, wlist(iw));
+                obj.wtoupdate(iw).msn = m;
+                obj.wtoupdate(iw).hvc = h;
+            end
+            
+            % Scale initial HVC-MSN weights
+            obj.wH(:,:,1) = obj.winit * obj.wH(:,:,1);
+            
             % Initialize vocal output
             obj.msnout = zeros(obj.nmsn, obj.nhvc, obj.niter);
             obj.lmanout = zeros(obj.nlman, obj.nhvc, obj.niter);
@@ -85,15 +101,13 @@ classdef SparseNetChanneled < SparseNetHardwired
             % Expected reward
             obj.rexp = zeros(obj.nhvc, obj.niter);
             % FIXME: Expected reward might be different? Initialize here.
-            
-            % FIXME: Is lateral inhibition local to each channel?
-            
         end
         
-        function ffstep(obj, iter)
-            obj.msnout(:,:,iter) = max(0, obj.wH(:,:,iter) * obj.hvcout); % MSN output
+        function lmanupdate(obj,iter)
             obj.lmanout(:,:,iter) = max(0, obj.wL' * obj.msnout(:,:,iter) + obj.noise(:,:,iter) + obj.lmanoffset); % LMAN output
-            % Vocal output
+        end
+        
+        function vocalupdate(obj, iter)
             obj.vocalout(:,:,iter) = obj.wV * obj.lmanout(:,:,iter);
         end
         
@@ -119,12 +133,10 @@ classdef SparseNetChanneled < SparseNetHardwired
             % Update weights between HVC and MSN. To save time and preserve
             % hardwired architecture, only update the weights that are not
             % zero.
-            wlist = find(obj.wH(:,:,iter) > 0); % list of weights to update
-            siz = size(obj.wH(:,:,iter));
-            for iw = wlist'
-                [imsn, ihvc] = ind2sub(siz, iw);
-                ilman = find(obj.wL(imsn,:) > 0);
-                L = obj.lmanout(ilman,:,iter);
+            for iw = 1:length(obj.wtoupdate)
+                imsn = obj.wtoupdate(iw).msn;
+                ihvc = obj.wtoupdate(iw).hvc;
+                L = obj.wL(imsn,:) * obj.lmanout(:,:,iter);
                 H = obj.hvcout(ihvc,:);
                 e = L .* H;
                 etrace = conv(e, obj.kernel);
