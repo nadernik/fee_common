8bfbdf298f2dd0be491fa252d5fd1393c764833b
Emily Mackevicius <elm@mit.edu>
2013-12-13 17:37:21 -0500

    adjusting durations

--- misc_elm/WhiteNoiseWithHoles.m
+++ misc_elm/WhiteNoiseWithHoles.m
@@ -1,8 +1,8 @@
 %% setup 
 fs = 44100;
 dur = .5;
-hf = design(fdesign.bandpass('N,F3dB1,F3dB2',200,5e3,20e3,fs));
-lf = design(fdesign.bandpass('N,F3dB1,F3dB2',200,20,3e3,fs));
+hf = design(fdesign.bandpass('N,F3dB1,F3dB2',100,6e3,20e3,fs));
+lf = design(fdesign.bandpass('N,F3dB1,F3dB2',100,20,2e3,fs));
 %doc fdesign.bandpass
 %Once you get the filter, assuming x is what you get from wgn, which is white noise, you can simply get the filtered noise by
 x = rand(1, fs*dur);
@@ -19,7 +19,8 @@ lo = filter(lf,x);
 % displaySpecgramQuick(hi+lo, fs)
 %%
 n = 5
+hl = hi+lo;
 for i = 1:n
-    sound(hi+lo, fs)
-    pause(rand*3*dur)
+    sound(hl(1:fs*.05), fs)
+    pause(rand*5*dur)
 end
