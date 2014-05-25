c3546791d20af4cd51fbc535b2f50ec1bc2ebb67
Galen Lynch <glynch@mit.edu>
2014-05-25 09:54:46 -0400

    Merging in electro_gui fork from Jesse and Galen

--- electro_gui/egm_Batch_segment.m
+++ electro_gui/egm_Batch_segment.m
@@ -2,6 +2,7 @@ function handles = egm_Batch_segment(handles)
 % ElectroGui macro
 % Batch syllable segmentation for faster analysis
 % Uses current segmentation algorithm and parameters
+% Only works for segmentation based on sound amplitude
 
 answer = inputdlg({'File range'},'File range',1,{['1:' num2str(handles.TotalFileNumber)]}); % default file range: all files
 if isempty(answer)
@@ -61,6 +62,7 @@ for j = 1:length(fls) % counter for files to be segmented
     end
     %---------------------------
     
+    handles.SegmentTimes{c} = eval(['egg_' alg '(amp,fs,curr,handles.SegmenterParams)']);
     handles.SegmentTitles{c} = cell(1,size(handles.SegmentTimes{c},1));
     handles.SegmentSelection{c} = ones(1,size(handles.SegmentTimes{c},1));
 
