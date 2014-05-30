70db629d71b51a112789e628fd52cc5b97e1e89d
Galen Lynch <glynch@mit.edu>
2014-05-30 15:33:21 -0400

    Removed commented out debug code from EGL_WAVEREAD.M

--- electro_gui/egl_WaveRead.m
+++ electro_gui/egl_WaveRead.m
@@ -2,17 +2,12 @@ function [data fs dateandtime label props] = egl_WaveRead(filename, loaddata)
 % ElectroGui file loader
 % Reads wavefiles
 % Extracts date and time information from the file info
-
-% currentFolder = pwd; 
-% [pathstr,name,ext] = fileparts(filename) 
 if loaddata == 1
-    %cd pathstr
     [data fs] = wavread(filename);
     data = mean(data,2);
     mt = dir(filename);
     dateandtime = datenum(mt(1).date);
     label = 'Sound level';
-    %cd currentFolder
 else
     data = [];
     fs = [];
