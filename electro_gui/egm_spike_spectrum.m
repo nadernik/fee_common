02e7407188de825e8cb2fa48c993cca6c723c78a
Maya <mayabr@mit.edu>
2014-05-30 14:49:12 -0400

    Added changes made by Maya

--- electro_gui/egm_spike_spectrum.m
+++ electro_gui/egm_spike_spectrum.m
@@ -2,28 +2,49 @@ function handles = spike_spectrum(handles)
 % ElectroGui macro
 % Plots the syllable distribution of all analyzed files
 
+% user dialog to select data (added by Maya):
+str=[] ; 
+ind = ones(1,2) ; 
+for c = 1:length(handles.EventTimes)
+    [param labels] = eval(['ege_' handles.EventDetectors{c} '(''params'')']);
+    for d = 1:length(labels)
+        str{end+1} = [handles.EventSources{c} ' - ' handles.EventFunctions{c} ' - ' labels{d}];
+        ind(length(str),1) = c;
+        ind(length(str),2) = d;
+    end
+end
+
+[iSelect,OK] = listdlg('PromptString','Please select spike source','ListString',str, 'ListSize',[300 300],'SelectionMode','single' ) ;
+
+if ~OK % if user pushes the 'cancel'button' 
+    return
+end
+
 filenum = str2num(get(handles.edit_FileNumber,'string')); % get current file number
-answer = inputdlg({'File','[winsize winstep]', '[minfreq maxfreq]', 'Event Index (order in sorted raster dropdown list)'},'in seconds...',1,{[num2str(filenum)],'[1.5 .1]', '[2 25]', '4'}); % input dialog box
+answer = inputdlg({'File','[winsize winstep]', '[minfreq maxfreq]'},'in seconds...',1,{[num2str(filenum)],'[1.5 .1]', '[2 25]'}); % input dialog box
 if isempty(answer)
     return
 end
-file = eval(answer{1}); % array of files to be analyzed, convert from string to number
+fFiles = eval(answer{1}); % array of files to be analyzed, convert from string to number
 
-movingwin = str2num(answer{2}); 
+movingwin = str2num(answer{2});
 fpass = str2num(answer{3});
-EventInd = str2num(answer{4});
 fs = handles.fs;
 figure; hold all
 
 %smoothing_window = .020; 
-spiketimes1 =handles.EventTimes{EventInd}{1, file};
-spiketimes = spiketimes1/fs;
+% spiketimes1 =handles.EventTimes{EventInd}{1, file};% in samples
+spiketimes1 = handles.EventTimes{ind(iSelect,1)}{ind(iSelect,2),fFiles}; % % in samples % User-selected spike source % maya
+ss =handles.EventSelected{ind(iSelect,1)}{ind(iSelect,2),fFiles}; % check for unselected events % Maya
+spiketimes1 = spiketimes1(logical(ss)) ;  % remove unselected events % maya
+
+spiketimes = spiketimes1/fs; % convert to spike time in sec
 spiketrain = zeros(1, numel(handles.sound));
-spiketrain(spiketimes1) = 1;
+spiketrain(spiketimes1) = 1; % a binary vector of spikes (ones and zeros)
 %filter = normpdf(-6*smoothing_window:1/fs:6*smoothing_window, 0, smoothing_window);
 %smooth_spiketimes = conv(spiketrain, filter);
 %sound(spiketrain, fs);
-time = (1:numel(handles.sound))/fs;
+time = (1:numel(handles.sound))/fs;% in sec
 %params.tapers = [.1 6];
 params.Fs = fs; 
 params.fpass = fpass;
@@ -32,9 +53,9 @@ data(1).times = spiketimes;
 %winsize = 1;
 %winstep = .1;
 %movingwin = [winsize winstep];
-[S,t,f] = mtspecgrampt(data, movingwin, params);
+[S,t,f] = mtspecgrampt(data, movingwin, params); % Multi-taper time-frequency spectrum - point process times
 %[S1,t1,f1] = mtspecgramc(smooth_spiketimes, movingwin, params);
-h = subplot(2,1,1)
+h = subplot(2,1,1);
 imagesc((S'), 'Xdata', t, 'Ydata', f)
 set(gca, 'ydir', 'normal')
 colormap hot
@@ -44,7 +65,7 @@ xlabel('time (s)')
 % imagesc(S1, 'Xdata', t1, 'Ydata', f1)
 % colormap hot
 % ylabel('frequency (Hz)')
-g = subplot(2,1,2)
+g = subplot(2,1,2);
 plot(time,spiketrain, 'k')
 linkaxes([h g], 'x')
 subplot(g)
--- toolbox/chronux/spectral_analysis/pointtimes/mtspecgrampt.m
+++ toolbox/chronux/spectral_analysis/pointtimes/mtspecgrampt.m
@@ -4,22 +4,22 @@ function [S,t,f,R,Serr]=mtspecgrampt(data,movingwin,params,fscorr)
 % Usage:
 %
 % [S,t,f,R,Serr]=mtspecgrampt(data,movingwin,params,fscorr)
-% Input: 
-%       data        (structure array of spike times with dimension channels/trials; 
+% Input:
+%       data        (structure array of spike times with dimension channels/trials;
 %                   also accepts 1d array of spike times) -- required
 %       movingwin         (in the form [window,winstep] i.e length of moving
 %                                                 window and step size.
-%                                                 
+%
 %       params: structure with fields tapers, pad, Fs, fpass, err, trialave
 %       - optional
 %           tapers : precalculated tapers from dpss or in the one of the following
-%                    forms: 
+%                    forms:
 %                    (1) A numeric vector [TW K] where TW is the
 %                        time-bandwidth product and K is the number of
 %                        tapers to be used (less than or equal to
-%                        2TW-1). 
+%                        2TW-1).
 %                    (2) A numeric vector [W T p] where W is the
-%                        bandwidth, T is the duration of the data and p 
+%                        bandwidth, T is the duration of the data and p
 %                        is an integer such that 2TW-p tapers are used. In
 %                        this form there is no default i.e. to specify
 %                        the bandwidth, you have to specify T and p as
@@ -31,7 +31,7 @@ function [S,t,f,R,Serr]=mtspecgrampt(data,movingwin,params,fscorr)
 %                        The default is to use form 1 with TW=3 and K=5
 %                     Note that T has to be equal to movingwin(1).
 %
-%	        pad		    (padding factor for the FFT) - optional (can take values -1,0,1,2...). 
+%	        pad		    (padding factor for the FFT) - optional (can take values -1,0,1,2...).
 %                    -1 corresponds to no padding, 0 corresponds to padding
 %                    to the next highest power of 2 etc.
 %			      	 e.g. For N = 500, if PAD = -1, we do not pad; if PAD = 0, we pad the FFT
@@ -39,17 +39,17 @@ function [S,t,f,R,Serr]=mtspecgrampt(data,movingwin,params,fscorr)
 %			      	 Defaults to 0.
 %           Fs   (sampling frequency) - optional. Default 1.
 %           fpass    (frequency band to be used in the calculation in the form
-%                                   [fmin fmax])- optional. 
+%                                   [fmin fmax])- optional.
 %                                   Default all frequencies between 0 and Fs/2
 %           err  (error calculation [1 p] - Theoretical error bars; [2 p] - Jackknife error bars
 %                                   [0 p] or 0 - no error bars) - optional. Default 0.
 %           trialave (average over trials/channels when 1, don't average when 0) - optional. Default 0
-%       fscorr   (finite size corrections, 0 (don't use finite size corrections) or 
+%       fscorr   (finite size corrections, 0 (don't use finite size corrections) or
 %                1 (use finite size corrections) - optional
 %                (available only for spikes). Defaults 0.
 %
 % Output:
-%       S       (spectrogram with dimensions time x frequency x channels/trials if trialave=0; 
+%       S       (spectrogram with dimensions time x frequency x channels/trials if trialave=0;
 %               dimensions time x frequency if trialave=1)
 %       t       (times)
 %       f       (frequencies)
@@ -89,17 +89,17 @@ else
 end
 
 for n=1:nw;
-   t=linspace(tn(n)-movingwin(1)/2,tn(n)+movingwin(1)/2,Nwin);
-   datawin=extractdatapt(data,[t(1) t(end)]);
-   if nargout==5;
-     [s,f,r,serr]=mtspectrumpt(datawin,params,fscorr,t);
-     Serr(1,n,:,:)=squeeze(serr(1,:,:));
-     Serr(2,n,:,:)=squeeze(serr(2,:,:));
-   else
-     [s,f,r]=mtspectrumpt(datawin,params,fscorr,t);
-   end;
-   S(n,:,:)=s;
-   R(n,:)=r;
+    t=linspace(tn(n)-movingwin(1)/2,tn(n)+movingwin(1)/2,Nwin);
+    datawin=extractdatapt(data,[t(1) t(end)]);
+    if nargout==5;
+        [s,f,r,serr]=mtspectrumpt(datawin,params,fscorr,t);
+        Serr(1,n,:,:)=squeeze(serr(1,:,:));
+        Serr(2,n,:,:)=squeeze(serr(2,:,:));
+    else
+        [s,f,r]=mtspectrumpt(datawin,params,fscorr,t);
+    end;
+    S(n,:,:)=s;
+    R(n,:)=r;
 end;
 t=tn;
 S=squeeze(S); R=squeeze(R); if nargout==5; Serr=squeeze(Serr);end
