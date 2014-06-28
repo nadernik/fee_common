1823ba1c595dc0d282eef08d12edb55575e74c14
Galen Lynch <glynch@mit.edu>
2014-06-28 15:12:05 -0400

    Added more information to event strings, original verison was too threadbare. Also added checks to menu selections to see if the user closed the window instead of responding.

--- electro_gui/egm_Match_event_selections.m
+++ electro_gui/egm_Match_event_selections.m
@@ -3,7 +3,13 @@ function handles = egm_Match_event_selections(handles)
 %then setting them to either be selected or deselected
 %
 %Written by Galen Lynch 6/24/2014
-detectorStrings = handles.EventDetectors;
+joinStr = ' - ';
+nDetectors = length(handles.EventDetectors);
+jnAr = cell(1, nDetectors);
+for dtctrNo = 1:nDetectors
+    jnAr{dtctrNo} = joinStr;
+end
+detectorStrings = strcat(handles.EventSources, jnAr, handles.EventFunctions, jnAr, handles.EventDetectors);
 if ~iscell(detectorStrings) || length(detectorStrings)<1
     errordlg('Must have at least 1 event type!','Error');
 else
@@ -12,8 +18,14 @@ else
     assert(size(handles.EventSelected{dtctrNo},1) == 2, 'An unusual event! Only event pairs are implemented ATM')
     %Ask what the default behavior should be
     detectMismatch = menu('How do you want to match events?', 'Match one set of the pair', 'Detect mismatches')-1;
+    if detectMismatch < 0
+        return
+    end
     if detectMismatch
         mismatchVal = menu('What should happen to mismatched event selections?', 'Deselect', 'Select')-1;
+        if mismatchVal < 0
+            return
+        end
         for fileNo = 1:size(handles.EventSelected{dtctrNo},2)
             if ~isempty(handles.EventSelected{dtctrNo}{1,fileNo})
                 mismatched = xor(handles.EventSelected{dtctrNo}{1,fileNo}, handles.EventSelected{dtctrNo}{2,fileNo});
@@ -26,6 +38,9 @@ else
         eventStrings = eventStrings(2:end);%Clip off 'none'
         eventStrings = eventStrings(2*dtctrNo-1:2*dtctrNo);%only get strings of selected detector
         pairChoice = menu('All pairs should match which selection set?', eventStrings{:});
+        if pairChoice < 1
+            return
+        end
         otherChoice = mod(2-pairChoice,2)+1;
         for fileNo = 1:size(handles.EventSelected{dtctrNo},2)
             if ~isempty(handles.EventSelected{dtctrNo}{1,fileNo})
