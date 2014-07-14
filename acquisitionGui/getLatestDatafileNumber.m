b1688c25ed145662dc2448df00496077790bbbca
Michael Stetner <stetner@mit.edu>
2014-07-14 14:43:21 -0400

    Fixed bug in getLatestDatafileNumber() where it would sometimes return an empty matrix

--- acquisitionGui/getLatestDatafileNumber.m
+++ acquisitionGui/getLatestDatafileNumber.m
@@ -1,6 +1,6 @@
 function num = getLatestDatafileNum(exper)
 num = 0;
-d = dir([exper.dir,exper.birdname,'_d*']);
+d = dir(fullfile(exper.dir, [exper.birdname, '_d*']));
 %for(i = 1:length(d))
 %    num = max(extractDatafileNumber(exper, d(i).name),num);
 %end
