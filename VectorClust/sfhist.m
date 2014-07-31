f5df07bf9b33caf103fc335b3e408edd1054395c
Michael Stetner <stetner@mit.edu>
2014-07-31 14:22:32 -0400

    sfhist.m added Stairs option to plot histogram using stairs instead of bar
    Stair plots look like the outline of bar plots. They are better when plotting
    two histograms on top of each other because one bar will not obscure another.

--- VectorClust/sfhist.m
+++ VectorClust/sfhist.m
@@ -6,25 +6,46 @@ function sfhist(vcdb, sfname, varargin)
 %   SFHIST(vcdb, sfname, 'Parameter', value, ...) takes additional
 %   parameters:
 %
-%       Mask = logical array to select the segments that are plotted
-%       BinWidth = width of bins in histogram. Bin edges are automatically
-%       calculated 
-%       PatchProperties = cell array of {'Parameter', value, ...} to be
-%       passed as arguments to histpp().
+%     Mask (default = trues)
+%       logical array to select the segments that are plotted. Its length
+%       must be equal to length(vcdb.d.v)
+%     BinWidth (default = [])
+%       Width of bins in histogram. If blank, the bin edges are 
+%       automatically calculated by the HIST function.
+%     PatchProperties (default = {})
+%       Cell array of {'Parameter', value, ...} to be passed as arguments 
+%       to histpp().
+%     Stairs (default = false)
+%       If true, plot histogram using STAIRS instead of HISTPP. Stairs is
+%       better if you want to make two overlapping histograms on the same
+%       plot because one stairs plot will not obscure another. If Stairs is
+%       true, the PatchProperties argument has no effect.
 %
-%   See also HISTPP
+%   See also HISTPP, STAIRS
 
 P.BinWidth = [];
 P.PatchProperties = [];
 P.Mask = true(size(vcdb.d.v));
+P.Stairs = false;
 P = parseargs(P, varargin{:});
 if isempty(P.PatchProperties)
     P.PatchProperties = {}; % because parseargs does not allow empty cell arrays
 end
 y = getsf(vcdb, sfname, P.Mask);
+
 if ~isempty(P.BinWidth)
     x = min(y - P.BinWidth):P.BinWidth:max(y + P.BinWidth);
-    histpp(y, x, P.PatchProperties{:})
+    if P.Stairs
+        n = hist(y, x);
+        stairs(x, n)
+    else
+        histpp(y, x, P.PatchProperties{:})
+    end
 else
-    histpp(y, P.PatchProperties{:})
+    if P.Stairs
+        [n, x] = hist(y);
+        stairs(x, n);
+    else
+        histpp(y, P.PatchProperties{:})
+    end
 end
