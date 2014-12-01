2e1bf31ae7ea3733732218b54963a56ed601d627
Emily Mackevicius <elm@mit.edu>
2014-12-01 11:46:23 -0500

    'shuffled' simulated data, to justify that it's significant when a simulated neuron fired >2x during a syllable.

---
+++ misc_elm/HVCmodel/testLatSig.m
@@ -0,0 +1,18 @@
+% testing what is a 'significant peak' in the raster to count a simulated
+% neuron as firing during a particular syllable; 
+
+nSyl = 4; % number of syllables of that type in bout; 
+m = 8; % number of timebins in that syllable; 
+nBurst = 1; % number of bursts/syllable; 
+
+for iter = 1:1000; 
+    simShuff = zeros(nSyl,m); 
+    for i = 1:nSyl;
+        tmp = randperm(m); 
+        simShuff(i,tmp(1)) = 1; 
+    end
+
+    raster = sum(simShuff);
+    M(iter) = max(raster); 
+end
+figure; hist(M, 0:5)
