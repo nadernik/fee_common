6f036de4432c4d2da97c78d5875b645808cc9522
Galen Lynch <glynch@mit.edu>
2015-02-23 18:07:46 -0500

    starting to try to shave time off of daq_bufferUpdate

--- daq/daq_bufferUpdate.m
+++ daq/daq_bufferUpdate.m
@@ -29,7 +29,7 @@ global PEEKTIMESTORE;
 
 FILEFORMATID = -4;
 daq_log(['numBufferUnits: ', num2str(NUMBUFFERUNITS)]);
-
+nBuff = bufferLength* bufferUnitSamps;
 %get data and add to buffer
 data = event.Data;
 time = event.TimeStamps;
@@ -53,7 +53,7 @@ for chanNo = 1:numel(BTRIGGER)
             if (TRIGGERSTART(chanNo)~= -1) && (NUMBUFFERUNITS * bufferUnitSamps >= TRIGGERSTART(chanNo))
                 if(TRIGGERSTART(chanNo) < max(NUMBUFFERUNITS*bufferUnitSamps-bufferUnitSamps*bufferLength+1,1))
                     warning('Trigger start outside buffer range.  Truncating start.');
-                    TRIGGERSTART(chanNo) = (NUMBUFFERUNITS*bufferUnitSamps) - min(NUMBUFFERUNITS*bufferUnitSamps, bufferUnitSamps*bufferLength) + 1;
+                    TRIGGERSTART(chanNo) = (NUMBUFFERUNITS*bufferUnitSamps) - min(NUMBUFFERUNITS*bufferUnitSamps, nBuff) + 1;
                 end
                 if(exist(TRIGGERFILENAME{chanNo}, 'file'))
                     warning('File already exists, data being appended to end of file.')
@@ -85,7 +85,7 @@ for chanNo = 1:numel(BTRIGGER)
                 %Next write the number of the first sample.
                 fwrite(TRIGGERFID(chanNo), TRIGGERSTART(chanNo), 'float64');
                 %Next write time of the first sample in the file (in seconds) since daq start
-                ndx = mod(trigStartNdx-1,length(GDAQTIME)) + 1;
+                ndx = mod(trigStartNdx-1,nBuff) + 1;
                 fwrite(TRIGGERFID(chanNo), GDAQTIME(ndx), 'float64');
                 
                 TRIGGERSTART(chanNo) = -1; %Signal that trigger has begun.
@@ -98,7 +98,7 @@ for chanNo = 1:numel(BTRIGGER)
                 triggerEndCopy = TRIGGEREND(chanNo);
                 TRIGGEREND(chanNo) = -1;
                 trigEndNdx = endNdx - (NUMBUFFERUNITS * bufferUnitSamps - triggerEndCopy);
-                ndx = mod( (trigStartNdx-1):(trigEndNdx-1) ,length(GDAQTIME)) + 1;
+                ndx = mod( (trigStartNdx-1):(trigEndNdx-1) ,nBuff) + 1;
                 fwrite(TRIGGERFID(chanNo), GDAQDATA(ndx,chanNo)', nativeDataType);
                 %Write the trigger file format id THREE times to
                 %mark the end of the window of samples.
@@ -108,7 +108,7 @@ for chanNo = 1:numel(BTRIGGER)
                 %Write the number and time of the last sample in the file
                 %for error checking.
                 fwrite(TRIGGERFID(chanNo), triggerEndCopy, 'float64');
-                ndx = mod( trigEndNdx-1 ,length(GDAQTIME)) + 1;
+                ndx = mod( trigEndNdx-1 ,nBuff) + 1;
                 fwrite(TRIGGERFID(chanNo), GDAQTIME(ndx), 'float64');
                 %Close the file.
                 fclose(TRIGGERFID(chanNo));
@@ -120,7 +120,7 @@ for chanNo = 1:numel(BTRIGGER)
                 %The latest update to the buffer includes unnessary data, so
                 %save on the desired part.  This should only happen on the
                 %first update during the trigger.
-                ndx = mod((trigStartNdx-1):(trigEndNdx-1), length(GDAQTIME)) + 1;
+                ndx = mod((trigStartNdx-1):(trigEndNdx-1), nBuff) + 1;
                 fwrite(TRIGGERFID(chanNo), GDAQDATA(ndx,chanNo)', nativeDataType);
             end
         end
@@ -134,14 +134,14 @@ if NPEEK
         warning('Cannot peek into future');
         PEEKDATASTORE = [];
         PEEKTIMESTORE = [];
-    elseif (NUMBUFFERUNITS * bufferUnitSamps - NPEEK) > (bufferUnitSamps * bufferLength)
+    elseif (NUMBUFFERUNITS * bufferUnitSamps - NPEEK) > (nBuff)
         NPEEK = 0;
         warning('Peek start is no longer in the buffer.');
         PEEKDATASTORE = [];
         PEEKTIMESTORE = [];
     else
         numSamples = NUMBUFFERUNITS * bufferUnitSamps - NPEEK + 1;
-        ndx = mod((endNdx-numSamples):(endNdx-1),length(GDAQTIME)) + 1;
+        ndx = mod((endNdx-numSamples):(endNdx-1),nBuff) + 1;
         PEEKDATASTORE = GDAQDATA(ndx,:);
         PEEKTIMESTORE = GDAQTIME(ndx);
         NPEEK = 0;
