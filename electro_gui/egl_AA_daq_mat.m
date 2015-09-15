b861243d17583b5d932e3d7c151c09cb9272d2d0
Michael Stetner <stetner@mit.edu>
2015-09-15 11:54:19 -0400

    egl_AA_daq_mat - added documentation and add support for getting ylabel from the save file

--- electro_gui/egl_AA_daq_mat.m
+++ electro_gui/egl_AA_daq_mat.m
@@ -1,11 +1,35 @@
-function [data fs dateandtime label props] = egl_AA_daq_mat(filename, loaddata)
+function [data, fs, dateandtime, label, props] = egl_AA_daq_mat(filename, loaddata)
+% egl_AA_daq_mat electro_gui loader for .mat files
+%
+% The .mat file must contain these variables:
+%   data = a vector of data
+%   info.fs = sampling rate for data, in Hz
+%   info.absStartTime = start time for this data file in datenum format
+%   info.propertyNames = cell array of property names
+%   info.propertyTypes = array of property values. Type can be:
+%                          1 for any string
+%                          2 for boolean
+%                          3 for a string chosen from a list
+%   info.propertyValues = cell array of property values
+%   info.label = OPTIONAL string ylabel for this data. If omitted, label 
+%                will be 'Voltage (mV)'
 
-label = 'Voltage (mV)';
+x = load(filename);
 
-load(filename);
+% In this format, it is optional to specify a label in the info struct. If
+% no label is specified, use the following default label.
+if ~isfield(x.info, 'label')
+    x.info.label = 'Voltage (mV)';
+end
 
-fs = info.fs;
-dateandtime = info.absStartTime;
-props.Names = info.propertyNames;
-props.Types = info.propertyTypes;
-props.Values = info.propertyValues;
+if loaddata == 0
+    x.data = [];
+end
+
+data         = x.data;
+fs           = x.info.fs;
+dateandtime  = x.info.absStartTime;
+label        = x.info.label;
+props.Names  = x.info.propertyNames;
+props.Types  = x.info.propertyTypes;
+props.Values = x.info.propertyValues;
