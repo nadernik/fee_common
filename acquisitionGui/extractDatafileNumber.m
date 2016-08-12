1e56b32f9eab9b3e4f3b43bb02c8842c23f09a36
Galen Lynch <glynch@mit.edu>
2016-08-11 22:47:13 -0400

    Improve acquisitionGui file number calculation
    acquisitionGui cannot use bird names containing underscore because file numbers
    are calculated by parsing file strings for underscores. I have replaced this
    manual string searching with a regular expression, which should allow for any
    bird name.

--- acquisitionGui/extractDatafileNumber.m
+++ acquisitionGui/extractDatafileNumber.m
@@ -6,13 +6,9 @@ function num = extractDatafileNumber(exper, name)
 % exper is the exper data structure created by acquisitionGui. You can load
 %       this with loadExper()
 % name is a string with the name of the datafile or a cell array of strings
-
+REGSTR = '^.+_d(\d{6})_\d{8}T\d{6}chan\d+\.dat$';
 if ~iscell(name)
     name = {name};
 end
-
-num = zeros(size(name));
-for k = 1:length(name)
-    u = strfind(name{k}, '_');
-    num(k) = str2num(name{k}(u(1)+2:u(2)-1));
-end
+tokens = regexp(name, REGSTR, 'tokens', 'once');
+num = str2double([tokens{:}]);
