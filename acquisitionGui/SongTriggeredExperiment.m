fcf210cc455d894a7173237ee26f271015439c14
Galen Lynch <glynch@mit.edu>
2016-11-04 08:56:00 -0400

    Add song detection parameters to SongTriggeredExperiment
    Also including a place to put defaults.

--- acquisitionGui/SongTriggeredExperiment.m
+++ acquisitionGui/SongTriggeredExperiment.m
@@ -1,32 +1,51 @@
 classdef SongTriggeredExperiment < handle
     properties
         %% Song detection parameters
-        windowSize
-        windowOverlap
         minFreq
         maxFreq
-        minNdx
-        maxNdx
         ratioThreshold
         songDuration
-        windowLength
-        windowAvg
-        durationThreshold
+        songDensity
+        daqParameters
     end
     properties (SetAccess = private)
+        %% Information about which channels are part of this experiment
+        songHWChannel
+        nonSongHWChannels
+        
+        %% Information about saved files
+        directory
         lastFileNo
         fileTimes
+        
+        %% Recording/monitoring status
         detectingSong
         forcedRecording
-        directory
     end
     properties (Access = private)
-        daqParameters
+        desiredDaqParameters
         recordingListener
+        
+        %% Derived song detection parameters
+        windowLength
+        windowAvg
+        minNdx
+        maxNdx
+        windowSize
+        windowOverlap
     end
     
     methods
-        function self = SongTriggeredExperiment()
+        function self = SongTriggeredExperiment(directory, songHWChannel, nonSongHWChannels, daqParameters, varargin)
+            persistent p;
+            if isempty(p)
+                p = inputParser();
+                    addParameter(p, 'minFreq', 2000);
+                    addParameter(p, 'maxFreq', 6000);
+                    addParameter(p, 'songDensity', 0.5); % aka durationThreshold
+                    addParameter(p, 'ratioThreshold', 2); % aka powerThreshold
+                    addParameter(p, 'songDuration', 1); % aka songLength
+            end
         end
     end
 end
