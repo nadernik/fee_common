3b83581ef405e5ceb2fedd1b1d03e8de136456f7
Michael Stetner <stetner@mit.edu>
2016-11-11 16:21:33 -0500

    loadExper - added optional argument to automatically correct exper.dir on loading

--- acquisitionGui/loadExper.m
+++ acquisitionGui/loadExper.m
@@ -1,15 +1,22 @@
-function exper = loadExper(birdname, expername, rootdir)
+function exper = loadExper(birdname, expername, rootdir, fixdir)
 % LOADEXPER loads acquisitionGui exper file
 %
 % Usage:
 %   exper = loadExper('birdname', 'expername')
+%       returns exper struct saved in .\birdname\expername\exper.mat
 %   exper = loadExper('birdname', 'expername', 'c:\path\to\data\')
-% 
-% If the path to data is omitted, the current directory is used.
-% Loads the file path\to\data\birdname\expername\exper.mat
+%       returns exper struct saved in c:\path\to\data\birdname\expername\exper.mat
+%   exper = loadExper('birdname', 'expername', 'c:\path\to\data\', 1)
+%       same as above, but also changes exper.dir to the directory containing
+%       exper.mat (c:\path\to\data\birdname\expername\). This is useful if the
+%       exper is not the same folder it was in at the time of recording.
 
 if(nargin < 3)
     rootdir = pwd;
 end
 
-load(fullfile(rootdir, birdname, expername, 'exper.mat'));    
+load(fullfile(rootdir, birdname, expername, 'exper.mat'));
+
+if nargin >= 4 && fixdir
+    exper.dir = fullfile(rootdir, birdname, expername);
+end
