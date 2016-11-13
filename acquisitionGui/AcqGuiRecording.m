fde465f246ff1d084465df0f5551a30756e4861a
Galen Lynch <glynch@mit.edu>
2016-11-13 17:44:57 -0500

    Rework AcqGuiRecording to load a file upon instantiation

--- acquisitionGui/AcqGuiRecording.m
+++ acquisitionGui/AcqGuiRecording.m
@@ -1,19 +1,28 @@
 classdef (Sealed) AcqGuiRecording < handle
-    % Class to load and store the files associated with a recording on disk
-    properties
-        maxLoadSize
-        samplesToLoad
-        fileNames
-        fileHwChans
-        audioSignal
-        nonSongSignals = cell(0, 1);
-        nonSongSignalNdx
+    %ACQGUIRECORDING loads and stores the files from a recording
+    %   It initially loads the audio file, and incrementally loads other
+    %   channels as they are requested.
+    properties (SetAccess = private)
+        recordingNo
+        
+        signalChannels
+        signals
         propertyNames
         propertyValues
+        fileFs
         fileCreationTime
+        
+        maxLoadSize
+        
+        fileNames
+        fileHwChans
+    end
+    properties (Access = private)
+        Experiment
+        samplesToLoad
     end
     methods
-        function self = AcqGuiRecording(varargin)
+        function self = AcqGuiRecording(Experiment, recordingNo, varargin)
             persistent p;
             if isempty(p)
                 p = inputParser();
@@ -22,22 +31,19 @@ classdef (Sealed) AcqGuiRecording < handle
             end
             parse(p, varargin{:});
             Params = p.Results;
+            self.Experiment = Experiment;
+            self.recordingNo = recordingNo;
             self.maxLoadSize = Params.maxLoadSize;
-        end
-        function load_recording(self)
-            %% Convenience variables
-            experNdx = self.currentExperNdx;
-            currExper = self.Experiments{self.currentExperNdx};
-            currDir = currExper.experDir;
             
             %% Reset signal variables
-            nChan = numel(self.nonSongHWChannels{experNdx});
-            self.nonSongSignals = cell(nChan, 1);
+            nChan = numel(self.Experiment.nonSongHWChannel) + 1;
+            self.signals = cell(nChan, 1);
+            self.signalChannels = nan(nChan, 1);
             
             %% Find data files
-            [relFileNames, self.fileHwChans] = currExper.find_files(recordingNo);
-            self.fileNames = fullfile(currDir, relFileNames);
-            songFile = self.fileNames{self.songHWChannels(experNdx) == hwChannels};
+            [relFileNames, self.fileHwChans] = self.Experiment.find_files(self.recordingNo);
+            self.fileNames = fullfile(self.Experiment.experDir, relFileNames);
+            songFile = self.fileNames{self.Experiment.songHWChannel == hwChannels};
             
             %% Determine if the recording is too big to load
             [~, info] = daq_readDatafile(songFile, true, 0);
@@ -48,31 +54,28 @@ classdef (Sealed) AcqGuiRecording < handle
             end
             
             %% Load the audio signal
-            [self.audioSignal, info] = daq_readDatafile(songFile, true, self.samplesToLoad);
+            self.signalChannels(1) = self.Experiment.songHWChannel;
+            if ~isempty(self.Experiment.nonSongHWChannels)
+                self.signalChannels(2:end) = self.Experiment.nonSongHWChannels;
+            end
+            [self.signals{1}, info] = daq_readDatafile(songFile, true, self.samplesToLoad);
             self.fileFs = info.fs;
             self.fileCreationTime = datetime(info.absStartTime, 'ConvertFrom', 'datenum');
             self.propertyNames = info.propertyNames;
             self.propertyValues = info.propertyValues;
-            
-            %% Load the other signals
-            for dispCh = 1:3
-                self.load_channel(self.experDisplayChannels(dispCh, experNdx));
-            end
-            
-            self.gui_spectrogram();
-            self.gui_file_properties();
-            self.gui_signals();
         end
         
-        function load_channel(self, hwChan)
-            experNdx = self.currentExperNdx;
-            nonSongChans = self.nonSongHWChannels{experNdx};
-            chanNdx = find(nonSongChans == hwChan, 1, 'first');
-            if ~isempty(chandNdx) && isempty(self.nonSongSignals{chanNdx}) % Still need to load this file
-                fileName = self.fileNames{self.fileHwChans == hwChan};
-                [self.nonSongSignals{chanNdx}, info] = ...
-                    daq_readDatafile(fileName, true, self.samplesToLoad);
-                assert(info.fs == self.fileFs, 'Different sampling frequency!');
+        function load_channel(self, hwChans)
+            nChan = numel(hwChans);
+            for chanNo = 1:nChan
+                thisChan = hwChans(chanNo);
+                chanNdx = find(self.signalChannels == thisChan, 1, 'first');
+                if ~isempty(chandNdx) && isempty(self.signals{chanNdx}) % Still need to load this file
+                    fileName = self.fileNames{self.fileHwChans == thisChan};
+                    [self.signals{chanNdx}, info] = ...
+                        daq_readDatafile(fileName, true, self.samplesToLoad);
+                    assert(info.fs == self.fileFs, 'Different sampling frequency!');
+                end
             end
         end
     end
