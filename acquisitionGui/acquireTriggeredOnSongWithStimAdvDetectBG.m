f833a47fca7e958c7de540d948c75db45a3b5a2d
Galen Lynch <glynch@mit.edu>
2016-11-14 22:49:27 -0500

    Remove a bunch of unused, seemingly duplicate functions
    This code looks like a precursor to acquisitionGui?

--- acquisitionGui/acquireTriggeredOnSongWithStimAdvDetectBG.m
+++
@@ -1,4 +0,0 @@
-function acquireTriggeredOnSongWithStimAdvDetectBG(exper, stimChan)
-
-timerAcqStart = timer('TimerFcn', {'CBacquireTriggeredOnSongWithStimAdvDetect', exper, stimChan});
-startat(timerAcqStart,now+((1/24)/3600));
