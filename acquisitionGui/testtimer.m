d0b29d943bc1f90abe21e6a3d48763f7674c6996
Galen Lynch <glynch@mit.edu>
2016-11-21 18:47:34 -0500

    Remove some unused functions

--- acquisitionGui/testtimer.m
+++
@@ -1,8 +0,0 @@
-function [h] = testtimer
-
-h = figure;
-set(h,'Tag', 'word');
-setappdata(h,'name',5);
-t = timer('TimerFcn','mycallback(t, [], findobj(''Tag'', ''word''))', 'Period', 1);
-set(t,'ExecutionMode','fixedDelay');
-start(t);
