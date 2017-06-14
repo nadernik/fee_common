4b0a66bf01d006563b6a0e99807e90f109dc2579
Emily Mackevicius <elm@mit.edu>
2017-06-14 15:26:49 -0400

    egl_CaRead_elm compatible with new format of calcium data files

--- electro_gui/egl_CaRead_elm.m
+++ electro_gui/egl_CaRead_elm.m
@@ -1,12 +1,19 @@
 function [data, fs, dateandtime, label, props] = egl_CaRead_elm(filename, loaddata)
 
 if loaddata == 1
-    load(filename, 'SOUND', 'SOUNDfs'); 
-    data = SOUND;
+    variableInfo = who('-file', filename);
+    if ismember('info', variableInfo) 
+        load(filename, 'info')
+        data = info.SOUND; 
+        fs = info.SOUNDfs; 
+    else 
+        load(filename, 'SOUND', 'SOUNDfs'); 
+        data = SOUND;
+        fs = SOUNDfs;
+    end
     mt = dir(filename);
     dateandtime = datenum(mt(1).date);
     label = 'Sound level';
-    fs = SOUNDfs; 
 else
     data = [];
     fs = [];
