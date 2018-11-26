80afb08289b63eafe398e6b435af1b5d5d59b79b
galenlynch <glynch@mit.edu>
2018-11-26 17:35:56 -0500

    Revert "Remove prompt"
    This reverts commit df70595e0ce1f882d37185e12ccc96067c513b44.

--- electro_gui/electro_gui.m
+++ electro_gui/electro_gui.m
@@ -1869,7 +1869,15 @@ else
     [file, path] = uigetfile('*.mat','Load analysis'); 
 end
 if length(file)<=1 % if you clicked cancel
-    return
+    answer = questdlg('Do you want to load a NIf row?');
+    if strcmp(answer, 'Yes')
+        % ask which row you want to
+        answer = inputdlg('Which row?');
+        [XLS, Columns] = loadNIfSpreadsheet_elm(); 
+        [dbase rowstr path file] = getDbase_elm(str2num(answer{1}), XLS, Columns);
+    else
+        return
+    end
 else
     if ~isstr(file)
         return % file name is not a string
