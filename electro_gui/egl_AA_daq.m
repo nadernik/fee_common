bc8526678451935725956a04ee943127b03a89c8
Galen Lynch <galen.lynch@gmail.com>
2019-05-09 12:26:15 -0400

    Remove trailing white space

--- electro_gui/egl_AA_daq.m
+++ electro_gui/egl_AA_daq.m
@@ -32,8 +32,8 @@ function [out1, out2, out3, out4, out5, out6, out7, out8] = daq_readDatafile_for
 %
 % Inputs: filename - string specifing the filename of the data file.
 %        bNewOutput - (optional) returns output in more understandable format.
-%        dataRange - (optional) empty([]) loads all samples, 0 load no samples, 
-%                               [startSamp, endSamp] loads the specifed range. (1 is the first sample).  
+%        dataRange - (optional) empty([]) loads all samples, 0 load no samples,
+%                               [startSamp, endSamp] loads the specifed range. (1 is the first sample).
 %
 % Old output format:
 %   [HWChannels, data, time, startSampNumber, names, values, trigFileFormat]
@@ -46,9 +46,9 @@ function [out1, out2, out3, out4, out5, out6, out7, out8] = daq_readDatafile_for
 %             channel number in daqchannels.
 % out2, info: Structure containing:
 %                   absStartTime: time at which file started (matlab time) (only approximate for fileformats -1 and -2)
-%                   startSampleNum: the number of the first sample in the file referenced to when data acq began.  (useful for resolving overlapping files) 
+%                   startSampleNum: the number of the first sample in the file referenced to when data acq began.  (useful for resolving overlapping files)
 %                   numSamples: the number of samples per channel in the file.
-%                   fs: the sampling rate in Hz. 
+%                   fs: the sampling rate in Hz.
 %                   daqchannels: the hardware channels recorded in the file
 %                   propertyNames: cell array containing property names
 %                   propertyTypes: cell array containing property types
@@ -72,19 +72,19 @@ function [out1, out2, out3, out4, out5, out6, out7, out8] = daq_readDatafile_for
 % out5, names: cell array of property names.
 % out6, values: cell array of property values.
 % out7, trigFileFormat: the fileFormat of the datafile.
-% out8, types: cell array of property types. 
+% out8, types: cell array of property types.
 %
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 % NOTES:
 % As of file format '-4', multiple sessions of recording cannot be appended
 % to the same file.
 %
-% As of file format '-2', if two saving sessions are appended into the 
+% As of file format '-2', if two saving sessions are appended into the
 % same file, this function will return the first session.
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-%%%%NOTE:  DO NOT CHANGE THE FILE FORMAT, 
-%%%%       WITHOUT INCREMENTING THE TRIGGER FILE FORMAT ID NUMBER!!!! 
+%%%%NOTE:  DO NOT CHANGE THE FILE FORMAT,
+%%%%       WITHOUT INCREMENTING THE TRIGGER FILE FORMAT ID NUMBER!!!!
 
 if(~exist(filename,'file'))
     error('File does not exist: %s', filename);
@@ -106,24 +106,24 @@ fid = fopen(filename);
 trigFileFormat = fread(fid, 1, 'float64');
 
 if(trigFileFormat == -4)
-    
-    %Read absolute clock times in datevec format, 6 number [Y,M,D,H,M,S]. 
+
+    %Read absolute clock times in datevec format, 6 number [Y,M,D,H,M,S].
     acquisitionStartTime = fread(fid, 6, 'float64');  %Time the daq was started.
     approxFileCreatedTime = fread(fid, 6, 'float64'); %Approximate time this file was writtin.
-    
+
     %Read the number of hardware channels
-	numHWChannels = fread(fid, 1, 'float64'); 
+	numHWChannels = fread(fid, 1, 'float64');
 
     %Read the hardware indices of the recorded channels.
 	HWChannels = fread(fid, numHWChannels, 'float64');
-    
+
     %Read information for converting data from native format to doubles
     %double data= (native data)*(native scaling constant) + (native offset constant)
     for(nChan = 1:numHWChannels)
-        nativeScale(nChan) =  fread(fid, 1, 'float64'); 
-        nativeOffset(nChan) =  fread(fid, 1, 'float64'); 
+        nativeScale(nChan) =  fread(fid, 1, 'float64');
+        nativeOffset(nChan) =  fread(fid, 1, 'float64');
     end
-    
+
     %Read the name of the native data format.
     nLetter = 1;
     while(true)
@@ -135,7 +135,7 @@ if(trigFileFormat == -4)
         nLetter = nLetter + 1;
     end
     nativeDataType = char(nativeDataType);
-    
+
     %reset fid position to just after data
     if(strcmp(nativeDataType,'double'))
         byteSize = 8;
@@ -146,11 +146,11 @@ if(trigFileFormat == -4)
     else
         error(['nativeDataType not checked for.  Add ', nativeDataType, ' to daq_readDatafile']);
     end
-    
+
     %Read internal daq timing for first sample in file.
     startSampNumber = fread(fid, 1, 'float64'); %The sample number.  First sample after daq was started is sample #1.
     startSampTime = fread(fid, 1, 'float64'); %The time the sample number in seconds from first sample. The function getdata return a time for each sample.
-            
+
     %Run through file backward until we reach the end of data marker
     fidDataStart = ftell(fid);
     fseek(fid, 0, 'eof');
@@ -166,22 +166,22 @@ if(trigFileFormat == -4)
             error('Failed to find mark');
         end
     end
-        
+
     %Read internal daq timing for last sample in file.
     endSampNumber = fread(fid, 1, 'float64');
-    endSampTime = fread(fid, 1, 'float64');        
+    endSampTime = fread(fid, 1, 'float64');
 
     %Read the requested data.
     if(length(dataRange)==1 && dataRange(1) == 0)
         %read no data.
         data = [];
-    else          
+    else
         %read specifed sample range
         if(isempty(dataRange))
             dataRange = [1,endSampNumber-startSampNumber+1];
         end
         if((length(dataRange)==2) && (dataRange(1)>0 && dataRange(1)<=dataRange(2) && dataRange(2)<=(endSampNumber-startSampNumber+1)))
-            fidNextPos = ftell(fid);         
+            fidNextPos = ftell(fid);
             fseek(fid, fidDataStart + (dataRange(1)-1)*byteSize*numHWChannels, 'bof');
             data = fread(fid, (dataRange(2)-dataRange(1)+1)*numHWChannels, nativeDataType);
 
@@ -191,9 +191,9 @@ if(trigFileFormat == -4)
             %Convert the daq native format to a double
             for(nChan = 1:numHWChannels)
                 data(:,nChan) = data(:,nChan)*nativeScale(nChan) + nativeOffset(nChan);
-            end   
+            end
 
-            fseek(fid, fidNextPos, 'bof');    
+            fseek(fid, fidNextPos, 'bof');
         else
             error('Invalid dataRange specified.')
         end
@@ -219,13 +219,13 @@ if(trigFileFormat == -4)
     end
 
     %close the file.
-    fclose(fid);    
-        
+    fclose(fid);
+
     %Setup output
     if(bUseNewOutput)
-        info.absStartTime = datenum(acquisitionStartTime') + startSampTime/(24*60*60); 
+        info.absStartTime = datenum(acquisitionStartTime') + startSampTime/(24*60*60);
         info.startSampleNum = startSampNumber;
-        info.daqchannels = HWChannels;        
+        info.daqchannels = HWChannels;
         info.numSamples = endSampNumber - startSampNumber + 1;
         info.fs = 1/ ((endSampTime - startSampTime) / (endSampNumber - startSampNumber));
         info.propertyNames = names;
@@ -237,7 +237,7 @@ if(trigFileFormat == -4)
         [out3,out4,out5,out6,out7,out8] = deal([]);
     else
         %Create time array:
-        time = [acquisitionStartTime', approxFileCreatedTime', startSampTime, endSampTime, endSampNumber];        
+        time = [acquisitionStartTime', approxFileCreatedTime', startSampTime, endSampTime, endSampNumber];
         out1 = HWChannels;
         out2 = data;
         out3 = time;
@@ -247,25 +247,25 @@ if(trigFileFormat == -4)
         out7 = trigFileFormat;
         out8 = types;
     end
-    
+
 elseif(trigFileFormat == -3)
-    %Read absolute clock times in datevec format, 6 number [Y,M,D,H,M,S]. 
+    %Read absolute clock times in datevec format, 6 number [Y,M,D,H,M,S].
     acquisitionStartTime = fread(fid, 6, 'float64'); %Time the daq was started.
     approxFileCreatedTime = fread(fid, 6, 'float64'); %Approximate time this file was writtin.
-    
+
     %Read the number of hardware channels
 	numHWChannels = fread(fid, 1, 'float64');
 
     %Read the hardware indices of the recorded channels.
 	HWChannels = fread(fid, numHWChannels, 'float64');
-    
+
     %Read information for converting data from native format to doubles
     %double data= (native data)*(native scaling constant) + (native offset constant)
     for(nChan = 1:numHWChannels)
         nativeScale(nChan) =  fread(fid, 1, 'float64');
         nativeOffset(nChan) =  fread(fid, 1, 'float64');
     end
-    
+
     %Read the name of the native data format.
     nLetter = 1;
     while(true)
@@ -277,7 +277,7 @@ elseif(trigFileFormat == -3)
         nLetter = nLetter + 1;
     end
     nativeDataType = char(nativeDataType);
-     
+
     %reset fid position to just after data
     if(strcmp(nativeDataType,'double'))
         byteSize = 8;
@@ -288,7 +288,7 @@ elseif(trigFileFormat == -3)
     else
         error(['nativeDataType not checked for.  Add ', nativeDataType, ' to daq_readDatafile']);
     end
-    
+
     %Read internal daq timing for first sample in file.
     startSampNumber = fread(fid, 1, 'float64'); %The sample number.  First sample after daq was started is sample #1.
     startSampTime = fread(fid, 1, 'float64'); %The time the sample number in seconds from first sample. The function getdata return a time for each sample.
@@ -296,21 +296,21 @@ elseif(trigFileFormat == -3)
     %Skip to end of file
     fidDataStart = ftell(fid);
     fseek(fid, -16, 'eof');
-    
+
     %Read internal daq timing for last sample in file.
     endSampNumber = fread(fid, 1, 'float64');
-    endSampTime = fread(fid, 1, 'float64');    
-    
+    endSampTime = fread(fid, 1, 'float64');
+
     %Read the requested data.
     if(length(dataRange)==1 && dataRange(1) == 0)
         %read no data.
         data = [];
-    else          
+    else
         %read specifed sample range
         if(isempty(dataRange))
             dataRange = [1,endSampNumber-startSampNumber+1];
         end
-        if((length(dataRange)==2) && (dataRange(1)>0 && dataRange(1)<=dataRange(2) && dataRange(2)<=(endSampNumber-startSampNumber+1)))      
+        if((length(dataRange)==2) && (dataRange(1)>0 && dataRange(1)<=dataRange(2) && dataRange(2)<=(endSampNumber-startSampNumber+1)))
             fseek(fid, fidDataStart + (dataRange(1)-1)*byteSize*numHWChannels, 'bof');
             data = fread(fid, (dataRange(2)-dataRange(1)+1)*numHWChannels, nativeDataType);
 
@@ -320,15 +320,15 @@ elseif(trigFileFormat == -3)
             %Convert the daq native format to a double
             for(nChan = 1:numHWChannels)
                 data(:,nChan) = data(:,nChan)*nativeScale(nChan) + nativeOffset(nChan);
-            end   
+            end
         else
             error('Invalid dataRange specified.')
         end
     end
 
     %close the file.
-    fclose(fid);    
-       
+    fclose(fid);
+
     %Setup output
     if(bUseNewOutput)
         info.absStartTime = datenum(acquisitionStartTime') + startSampTime/(24*60*60);
@@ -345,7 +345,7 @@ elseif(trigFileFormat == -3)
         [out3,out4,out5,out6,out7,out8] = deal([]);
     else
         %Create time array:
-        time = [acquisitionStartTime', approxFileCreatedTime', startSampTime, endSampTime, endSampNumber];        
+        time = [acquisitionStartTime', approxFileCreatedTime', startSampTime, endSampTime, endSampNumber];
         out1 = HWChannels;
         out2 = data;
         out3 = time;
@@ -355,64 +355,64 @@ elseif(trigFileFormat == -3)
         out7 = trigFileFormat;
         out8 = {};
     end
-    
+
 elseif(trigFileFormat == -2)
-            
+
  	%Parse the first float64 as the number of channels
-	startSampNumber = fread(fid, 1, 'float64');   
-    
+	startSampNumber = fread(fid, 1, 'float64');
+
 	%Parse the second float64 as the number of channels
 	numHWChannels = fread(fid, 1, 'float64');
-	
+
 	%Read the hardware indices of the recorded channels.
-	HWChannels = fread(fid, numHWChannels, 'float64'); 
-	
+	HWChannels = fread(fid, numHWChannels, 'float64');
+
     %Extract number of samples and sampling rate.
     fidDataStart = ftell(fid);
     fread(fid, numHWChannels, 'float64');
     startSampTime = fread(fid, 1, 'float64');
     fseek(fid,-16,'eof'); %go back two values: the -2 marker and last time sample.
-    endSampTime = fread(fid, 1, 'float64');    
+    endSampTime = fread(fid, 1, 'float64');
     fidDataEnd = ftell(fid);
-    
+
     numSamples = ((fidDataEnd - fidDataStart)/8) / (numHWChannels+1);
     fs = 1/((endSampTime - startSampTime)/(numSamples-1));
-    
+
     %Read the requested data.
     if(length(dataRange)==1 && dataRange(1) == 0)
         %read no data.
         data = [];
-    else          
+    else
         %read specifed sample range
         if(isempty(dataRange))
             dataRange = [1,numSamples];
         end
-        if((length(dataRange)==2) && (dataRange(1)>0 && dataRange(1)<=dataRange(2) && dataRange(2)<=numSamples))      
+        if((length(dataRange)==2) && (dataRange(1)>0 && dataRange(1)<=dataRange(2) && dataRange(2)<=numSamples))
             fseek(fid, fidDataStart + (dataRange(1)-1)*8*(numHWChannels+1), 'bof');
             data = fread(fid, (dataRange(2)-dataRange(1)+1)*(numHWChannels+1), 'float64');
         else
             error('Invalid dataRange specified.')
         end
     end
-	
+
     %Reshape the datafile so that each column represents a channel.
     data = reshape(data, numHWChannels+1, length(data)/(numHWChannels+1))';
-    
+
 	%Break off the last column as the time stamp for each sample.
 	time = data(:,end);
 	data = data(:,1:end-1);
-    
+
     %close the file.
-    fclose(fid);  
-    
+    fclose(fid);
+
     %Setup output
     if(bUseNewOutput)
         [junk, sndx] = regexp(filename, '_d\d\d\d\d\d\d_');
         endx = regexp(filename, 'chan\d');
         strTimeCreated = filename(sndx(end)+1:endx(end)-1);
-        info.absStartTime = datenum(strTimeCreated,'yyyymmddTHHMMSS'); %approximate        
+        info.absStartTime = datenum(strTimeCreated,'yyyymmddTHHMMSS'); %approximate
         info.startSampleNum = startSampNumber;
-        info.daqchannels = HWChannels;        
+        info.daqchannels = HWChannels;
         info.numSamples = numSamples;
         info.fs = fs;
         info.propertyNames = {};
@@ -433,30 +433,30 @@ elseif(trigFileFormat == -2)
         out7 = trigFileFormat;
         out8 = {};
     end
-    
+
 elseif(trigFileFormat == -1)
     %read remainder of file.
     data = fread(fid, inf, 'float64');
-    fclose(fid);    
-    
+    fclose(fid);
+
  	%Parse the second float64 as startSample number.  Useful for orienting
  	%across files.
-	startSampNumber = data(1);   
-    
+	startSampNumber = data(1);
+
 	%Parse the second float64 as the number of channels
 	numHWChannels = data(2);
-	
+
 	%Read the hardware indices of the recorded channels.
 	HWChannels = data(3:3+numHWChannels-1);
-	
+
 	%Reshape the datafile so that each column represents a channel.  (Time
 	%is stored as the last channel, so add one).
 	data = reshape(data(3+numHWChannels:end), numHWChannels+1, length(data(3+numHWChannels:end))/(numHWChannels+1))';
-	
+
 	%Break off the last column as the time stamp for each sample.
 	time = data(:,end);
 	data = data(:,1:end-1);
-    
+
     %Extract requested region
     if(length(dataRange)==1 && dataRange(1)==0)
         dataclip = [];
@@ -465,7 +465,7 @@ elseif(trigFileFormat == -1)
         dataclip = data([dataRange(1):dataRange(2)],:);
         timeclip = time([dataRange(1):dataRange(2)]);
     end
-    
+
     %Setup output
     if(bUseNewOutput)
         [junk, sndx] = regexp(filename, '_d\d\d\d\d\d\d_');
@@ -493,7 +493,7 @@ elseif(trigFileFormat == -1)
         out6 = {};
         out7 = trigFileFormat;
         out8 = {};
-    end    
+    end
 else
     error(['Unknown trigger-file format:', num2str(trigFileFormat)]);
 end
