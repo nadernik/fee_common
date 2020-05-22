68ebe7d04c388a9c6c0d1b18462ac1f38d33383d
Galen Lynch <galen.lynch@gmail.com>
2020-05-21 21:34:34 -0400

    Add egl loader for feather daq files

---
+++ electro_gui/egl_feather_daq.m
@@ -0,0 +1,22 @@
+function [data, fs, dateandtime, label, Props] = egl_feather_daq(filename, loadData)
+
+label = 'Voltage (V)';
+toks = regexp(filename, '^.*audioOut_(\d{4}-\d{2}-\d{2}T\d{2}_\d{2}_\d{2}\.\d{7}).*$', 'tokens', 'once');
+if isempty(toks)
+    error("unable to parse file name for date and time");
+end
+
+dt = datetime(toks{1}, 'InputFormat', 'yyyy-MM-dd''T''HH_mm_ss.SSSSSSS');
+dateandtime = datenum(dt);
+
+if loadData ~= 1
+    data = [];
+else
+    fid = fopen(filename, 'r');
+    rawdata = fread(fid, [3, Inf], 'double');
+    data = rawdata(1, :);
+end
+fs = 48000;
+Props.Names = {'Comment'};%made up properties because I don't understand what they should be
+Props.Types = 1;
+Props.Values = {''};
