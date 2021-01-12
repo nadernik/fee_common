99e45520324aef14df1cbb2a72e3978da607068c
Galen Lynch <galen@galenlynch.com>
2021-01-12 08:53:07 -0500

    Remove nif row hook from electro_gui

--- electro_gui/electro_gui.m
+++ electro_gui/electro_gui.m
@@ -1868,23 +1868,11 @@ if isfield(handles, 'path_name')
 else
     [file, path] = uigetfile('*.mat','Load analysis'); 
 end
-if length(file)<=1 % if you clicked cancel
-    answer = questdlg('Do you want to load a NIf row?');
-    if strcmp(answer, 'Yes')
-        % ask which row you want to
-        answer = inputdlg('Which row?');
-        [XLS, Columns] = loadNIfSpreadsheet_elm(); 
-        [dbase rowstr path file] = getDbase_elm(str2num(answer{1}), XLS, Columns);
-    else
-        return
-    end
-else
-    if ~isstr(file)
-        return % file name is not a string
-    end
-
-    load(fullfile(path, file),'dbase'); % load variable 'dbase'
+if ~isstr(file)
+    return % file name is not a string
 end
+
+load(fullfile(path, file),'dbase'); % load variable 'dbase'
 
 handles.BackupChan = cell(1,2);
 handles.BackupLabel = cell(1,2);
